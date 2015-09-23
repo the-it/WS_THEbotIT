949600d86b858511191c10c56baea1f9415469dc
Erik Sommer <ersotech@posteo.de>
2015-09-23 20:39:52 +0200

    Bilderdownloadskript

---
+++ scripts/offline_scripts/mass_download_bilder/download.py
@@ -0,0 +1,10 @@
+__author__ = 'eso'
+
+import requests
+
+for i in range(813, 815):
+    print(i)
+    picture = requests.get(url='http://bibliothek.bbaw.de/DAS_jpg/07-abh/18201821/jpg-1000/00000' + str(i) + '.jpg', headers={'User-Agent': 'Python-urllib/3.1'}, timeout=2)
+    fobj = open("b_"+ str(i) +".jpg", "wb")
+    fobj.write(picture.content)
+    fobj.close()
