877ea31fa8dd04aebb14f2abb29d55dc66f5a506
Erik Sommer <ersotech@posteo.de>
2015-11-02 18:14:08 +0100

    test_online leert alle Testseiten

--- scripts/online_scripts/test_online.py
+++ scripts/online_scripts/test_online.py
@@ -2,10 +2,10 @@
 __author__ = 'eso'
 import sys
 sys.path.append('../../')
-import re
-import requests
 import pywikibot
 
 site = pywikibot.Site('de', 'wikisource')
-page = pywikibot.Page(site, 'Benutzer:THEbotIT/Test1')
-page.save()
+for i in range(11):
+    page = pywikibot.Page(site, 'Benutzer:THEbotIT/Test' + str(i))
+    page.text = ""
+    page.save(summary="clean the page", botflag=True)
