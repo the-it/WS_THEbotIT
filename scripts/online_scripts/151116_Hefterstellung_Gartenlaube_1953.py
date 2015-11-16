d80e161790090a01183e4a2c7a7ab8673f31dfe0
Erik Sommer <ersotech@posteo.de>
2015-11-17 00:16:35 +0100

    Heftanlage Gartenlaube 1953

---
+++ scripts/online_scripts/151116_Hefterstellung_Gartenlaube_1953.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+__author__ = 'eso'
+import sys
+sys.path.append('../../')
+from tools.wiki_template_handler.template_handler import TemplateHandler
+import re
+import pywikibot
+
+first_page = [1, 9, 21, 33, 45, 55, 67, 75, 87, 97, 109, 121, 133, 143, 155, 167, 177, 189, 199, 207, 219, 229, 241, 251,
+              263, 273, 285, 297, 309, 321, 331, 343, 355, 363, 373, 383, 393, 405, 417, 429, 441, 453, 463, 475, 485,
+              497, 509, 521, 533, 545, 557, 569]
+
+wiki = pywikibot.Site()
+
+
+pass
