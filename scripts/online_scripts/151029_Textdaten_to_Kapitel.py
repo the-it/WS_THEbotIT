052621a91bb87d6fd1385f92f95c163495fac1fc
Erik Sommer <ersotech@posteo.de>
2015-12-02 10:48:12 +0100

    Korrektur zur Runtime

--- scripts/online_scripts/151029_Textdaten_to_Kapitel.py
+++ scripts/online_scripts/151029_Textdaten_to_Kapitel.py
@@ -7,36 +7,7 @@ import re
 import pywikibot
 
 
-lemmas = ["De vulgari eloquentia/I. Buch – Achtes Kapitel",
-"De vulgari eloquentia/I. Buch – Achtzehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Dreizehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Drittes Kapitel",
-"De vulgari eloquentia/I. Buch – Elftes Kapitel",
-"De vulgari eloquentia/I. Buch – Erstes Kapitel",
-"De vulgari eloquentia/I. Buch – Fünftes Kapitel",
-"De vulgari eloquentia/I. Buch – Fünfzehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Neuntes Kapitel",
-"De vulgari eloquentia/I. Buch – Neunzehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Sechstes Kapitel",
-"De vulgari eloquentia/I. Buch – Sechszehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Siebentes Kapitel",
-"De vulgari eloquentia/I. Buch – Siebzehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Viertes Kapitel",
-"De vulgari eloquentia/I. Buch – Vierzehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Zehntes Kapitel",
-"De vulgari eloquentia/I. Buch – Zweites Kapitel",
-"De vulgari eloquentia/I. Buch – Zwölftes Kapitel",
-"De vulgari eloquentia/II. Buch – Achtes Kapitel",
-"De vulgari eloquentia/II. Buch – Dreizehntes Kapitel",
-"De vulgari eloquentia/II. Buch – Drittes Kapitel",
-"De vulgari eloquentia/II. Buch – Elftes Kapitel",
-"De vulgari eloquentia/II. Buch – Erstes Kapitel",
-"De vulgari eloquentia/II. Buch – Fünftes Kapitel",
-"De vulgari eloquentia/II. Buch – Neuntes Kapitel",
-"De vulgari eloquentia/II. Buch – Sechstes Kapitel",
-"De vulgari eloquentia/II. Buch – Siebentes Kapitel",
-"De vulgari eloquentia/II. Buch – Viertes Kapitel",
-"De vulgari eloquentia/II. Buch – Vierzehntes Kapitel",
+lemmas = ["De vulgari eloquentia/II. Buch – Vierzehntes Kapitel",
 "De vulgari eloquentia/II. Buch – Zehntes Kapitel",
 "De vulgari eloquentia/II. Buch – Zweites Kapitel",
 "De vulgari eloquentia/II. Buch – Zwölftes Kapitel"]
@@ -52,13 +23,16 @@ for lemma in lemmas:
     template_navigation.set_title('Kapitel')
 
     page_from_orig = template_handler_orig.get_parameter('HERKUNFT')['value']
-    page_from_orig = re.search(r'\d{1,3}–\d{1,3}', page_from_orig).group()
+    page_from_orig = re.search(r'\d{1,3}(?:–\d{1,3})?', page_from_orig).group()
 
     list_navigation = []
     list_navigation.append({'key': 'HERKUNFT', 'value': "De vulgari eloquentia"})
     list_navigation.append({'key': 'SEITE', 'value': page_from_orig})
-    list_navigation.append({'key': 'VORIGER', 'value': re.sub('De vulgari eloquentia/', '', template_handler_orig.get_parameter('VORIGER')['value'])})
-    list_navigation.append({'key': 'NÄCHSTER', 'value': re.sub('De vulgari eloquentia/', '', template_handler_orig.get_parameter('NÄCHSTER')['value'])})
+    try:
+        list_navigation.append({'key': 'VORIGER', 'value': re.sub('De vulgari eloquentia/', '', template_handler_orig.get_parameter('VORIGER')['value'])})
+        list_navigation.append({'key': 'NÄCHSTER', 'value': re.sub('De vulgari eloquentia/', '', template_handler_orig.get_parameter('NÄCHSTER')['value'])})
+    except:
+        pass
     list_navigation.append({'key': 'TITELTEIL', 'value': "2"})
     #list_navigation.append({'key': 'BILD', 'value': template_handler_orig.get_parameter('BILD')['value']})
     list_navigation.append({'key': 'BEARBEITUNGSSTAND', 'value': template_handler_orig.get_parameter('BEARBEITUNGSSTAND')['value']})
