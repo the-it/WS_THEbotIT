fac70c65ca923cff4bfeedc59a98ad96b36c65a1
Erik Sommer <ersotech@posteo.de>
2016-03-11 12:33:24 +0100

    basics for sortbot

--- test_the_bot_it.py
+++ test_the_bot_it.py
@@ -1,20 +1,26 @@
-__author__ = 'eso'
-
 import requests
-import json
-import pprint
-import time
-import datetime
 import re
-from pywikibot import pagegenerators
-import pywikibot
+import unicodedata
 
-if __name__ == "__main__":
-    wiki = pywikibot.Site()
-    for i in range(10):
-        seite = pywikibot.Page(wiki, title='Seite:Loos Sämtliche Schriften.pdf/62', ns=0)
-        list_ver = seite.getVersionHistory(reverse=True)
-        test = seite.backlinks()
-        for i in test:
-            print(i)
-        print('load the site{}'.format(seite.text))
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letters = "AB"
+
+dict_for_symbols = {}
+
+for letter in letters:
+    print(letter)
+    dict_for_symbols.update({letter: []})
+    dict_for_symbols.update({letter.lower(): []})
+    response = requests.get(url="http://www.fileformat.info/info/unicode/char/search.htm?q={}&preview=none".format(letter), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}, timeout=30)
+    matches = re.findall('<td><a href="\d{4}\/index\.htm">([\w\s]*)<\/a><\/td>', response.text)
+    for match in matches:
+        try:
+            unicode_letter = unicodedata.lookup(match)
+            print(re.search("CAPITAL", match), match)
+            if re.search("CAPITAL", match):
+                print(dict_for_symbols[letter].append(unicode_letter))
+                print(dict_for_symbols[letter])
+                dict_for_symbols.update({letter: dict_for_symbols[letter].append(unicode_letter)})
+
+        except:
+            print("{} doesnt exist".format(match))
---
+++ tools/sort_converter.py
@@ -0,0 +1,94 @@
+__author__ = 'Erik Sommer'
+
+import re
+
+symbol_mapping = {   " ÁÀÂÄÃÅǍĀĂ":    'A'}
+
+
+class DateConversion:
+    """
+
+    """
+    def __init__(self, rawstring):
+        self.rawstring = rawstring
+
+    def  __str__(self):
+        #chop the unused parts of the string
+        str_re_form = self._chop_ref(self.rawstring)
+
+        #inspect the string
+        match_dont_known = re.search('(unbekannt|Unbekannt)', str_re_form)
+        match_before_domino = re.search('v. Chr', str_re_form)
+        match_only_century = re.search('\d{1,2}\. Jahrhundert', str_re_form)
+        match_complete_date = re.search('\d{1,2}(\.|\. | )(\d\d?|Jan|Jän|Feb|Mär|Apr|Mai|Jun|Jul|Aug|Sep|Okt|Nov|Dez)\w*(\.|\. | )(\d{1,4})', str_re_form)
+        match_no_day = re.search('(\d{1,2}\.|Jan|Jän|Feb|Mär|Apr|Mai|Jun|Jul|Aug|Sep|Okt|Nov|Dez)\w*(\.|\. | )(\d{1,4})', str_re_form)
+        match_only_year = re.search('\d{1,4}', str_re_form)
+
+        #sort for structure of the information and interpred it
+        if str_re_form == '' or match_dont_known:
+            # Case: empty rawstring
+            str_re_form = '!-00-00'
+        elif match_only_century:
+            # Case: only a century given
+            century = re.search('\d{1,2}', match_only_century.group())
+            if match_before_domino:
+                century = int(century.group())
+            else:
+                century = int(century.group())-1
+            year = self._append_zeros_to_year(str(century) + '00')
+            str_re_form = ''.join([year, '-', '00', '-', '00'])
+            del year
+        elif match_complete_date:
+            # Case: complete date
+            li_str = re.split('\.?[ ]?', match_complete_date.group())
+            li_str[0] = re.sub('\.', '', li_str[0]) # Punkt aus dem Tag entfernen
+            li_str[1] = self._month_to_int(li_str[1])  #Monat in Zahl verwandeln
+            li_str[2] = self._append_zeros_to_year(li_str[2]) #append zeros to the year
+            str_re_form = ''.join([li_str[2], '-', li_str[1], '-', li_str[0]])
+        elif match_no_day:
+            # Case: only month and year
+            li_str = re.split(' ', match_no_day.group())
+            li_str[0] = self._month_to_int(li_str[0])  #Monat in Zahl verwandeln
+            li_str[1] = self._append_zeros_to_year(li_str[1]) #append zeros to the year
+            str_re_form = ''.join([li_str[1], '-', li_str[0], '-', '00'])
+        elif match_only_year:
+            # Case: only year
+            li_str = re.split(' ', match_only_year.group())
+            li_str[0] = self._append_zeros_to_year(li_str[0]) #append zeros to the year
+            str_re_form = ''.join([li_str[0], '-', '00', '-', '00'])
+        else:
+            raise ValueError(str_re_form)
+
+        # interpret the information of v. Chr.
+        if match_before_domino:
+            year = int(str_re_form[0:4])
+            converted_year = 9999 - year
+            str_re_form = '-' + self._append_zeros_to_year(str(converted_year)) + str_re_form[4:]
+
+        return str_re_form
+
+    @ staticmethod
+    def _chop_ref(rawstring):
+        str_re_value = re.sub('<ref>.+</ref>', '', rawstring)
+        str_re_value = re.sub(r'\{\{CRef\|.+\}\}', '', str_re_value)
+        return str_re_value
+
+    @ staticmethod
+    def _append_zeros_to_year(year):
+        for i in range((4 - len(year))):
+            year = '0' + year
+        return year
+
+    @ staticmethod
+    def _month_to_int(month):
+        try:
+            month_int = int(month)
+            if month_int < 10:
+                return '0' + str(month_int)
+            else:
+                return str(month_int)
+        except Exception:
+            try:
+                return month_mapping[month]
+            except:
+                raise ValueError(month)
