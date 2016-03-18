6a5dd522b770d7bddc6a9e5764702d01fb39b6ec
Erik Sommer <ersotech@posteo.de>
2016-03-18 14:39:15 +0100

    script for compiling RE authorlist ... with marks for lifedata

---
+++ scripts/online_scripts/160318_RE_author.py
@@ -0,0 +1,28 @@
+import re
+import pywikibot
+
+def frame_year(year):
+    if int(year.group(0)) > 1945:
+        return "'''" + year.group(0) + "'''"
+    else:
+        return year.group(0)
+
+wiki = pywikibot.Site()
+page = pywikibot.Page(wiki, "Paulys Realencyclopädie der classischen Altertumswissenschaft/Autoren")
+raw = page.text
+
+finding = re.findall("\|-\n\|(.*)\n\|(.*)", raw)
+newstring = []
+
+for item in finding:
+    newstring.append("* ")
+    newstring.append(item[0])
+    newstring.append(" ")
+    newstring.append(item[1])
+    newstring.append("\n")
+raw_list = "".join(newstring)
+fit = re.compile("\d{4}")
+temp = fit.sub(lambda x: frame_year(x), raw_list)
+page = pywikibot.Page(wiki, "Benutzer:THE IT/RE author")
+page.text = '<div style="-moz-column-count:10; -webkit-column-count:10; column-count:10; -moz-column-width: 250px; -webkit-column-width:250px; column-width:250px;">\n' + temp + "</div>"
+page.save()
