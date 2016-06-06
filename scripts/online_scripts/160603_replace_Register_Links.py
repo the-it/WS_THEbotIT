15f3cfb4255c60c9d37a9f99a4f4544c36563f6b
Erik Sommer <ersotech@posteo.de>
2016-06-06 11:08:23 +0200

    registerconverter .. final

--- scripts/online_scripts/160603_replace_Register_Links.py
+++ scripts/online_scripts/160603_replace_Register_Links.py
@@ -8,14 +8,19 @@ import re
 
 fit_numbers = re.compile("(?:''')?(\d{1,3}[a-z]?\))") #matches all numbers
 fit_numbers_not_linked = re.compile(" (?:''')?(\d{1,3}[a-z]?\))") #matches only the numbers no already linked
-fit_keyword = re.compile("^(?:''')?(?:\[\[RE:)?(?:''')?([A-Za-z][äÄöÖüÜßa-z\(\)\?]{1,20})(?:''')?[,\| ]?")  #matches all keywords
-fit_keyword_not_linked = re.compile("^(?:''')?([A-Za-z][äÄöÖüÜßa-z\(\)\?]{1,20})(?:''')?")
+fit_keyword = re.compile("^(?:''')?(?:\[\[RE:)?(?:''')?([A-Za-z][äÄöÖüÜßa-zè\(\)\?]{1,20})(?:''')?[,\| ]?")  #matches all keywords
+fit_keyword_not_linked = re.compile("^(?:''')?([A-Za-z][äÄöÖüÜßa-zè\(\)\?]{1,20})(?:''')?")
 
 def convert_one_link(hit, keyword):
-    return '[[RE:' + keyword + '|' + hit.group(1) + ']]'
+    if re.search("'''.*'''", hit.group(0)):
+        return "'''" + '[[RE:' + keyword + '|' + hit.group(1) + ']]' + "'''"
+    else:
+        return '[[RE:' + keyword + '|' + hit.group(1) + ']]'
 
 def convert_numbers(hit, keyword):
-    return '[[RE:' + keyword + ' ' + '' +'|' + hit.group(1) + ']]'
+    offset = len(hit.group(0)) - len(hit.group(1))
+
+    return hit.group(0)[0:offset] + '[[RE:' + keyword + ' ' + hit.group(1)[0:-1] +'|' + hit.group(1) + ']]'
 
 def convert_line(line):
     if fit_keyword.search(line):
@@ -29,7 +34,8 @@ def convert_line(line):
     return line
 
 if __name__ == "__main__":
-    with open('dump/Register.txt', encoding='utf8') as filepointer:
-        lines = filepointer.readlines()
-        for line in lines:
-            print(convert_line(line))
+    with open('dump/Regist_convert.txt', mode='w', encoding='utf8') as writepointer:
+        with open('dump/Register.txt', encoding='utf8') as filepointer:
+            lines = filepointer.readlines()
+            for line in lines:
+                writepointer.write(convert_line(line))
