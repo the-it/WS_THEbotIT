6fa89783380d4b24211f48cdd1b1b247f4b6e57f
Erik Sommer <ersotech@posteo.de>
2016-07-18 17:24:32 +0200

    new project status for GL - variant of the RE script - runner.py new structure

--- scripts/runner.py
+++ scripts/runner.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + os.pardir
 
 from scripts.service_scripts.authorlist import AuthorList
 from scripts.service_scripts.project_status_RE import REStatus
+from scripts.service_scripts.project_status_GL import GLStatus
 from tools.bots import PingBot, SaveExecution
 from tools.little_helpers import load_password
 
@@ -23,10 +24,12 @@ if __name__ == "__main__":
         login.login()
 
     # daily bots
-    run_bot(PingBot(wiki))
     run_bot(AuthorList(wiki))
-    run_bot(REStatus(wiki))
 
     # tasks for sunday
     if datetime.now().weekday() == 6:
-        pass
+        run_bot(REStatus(wiki))
+
+    # tasks for the first day of the month
+    if datetime.now().day == 1:
+        run_bot(GLStatus(wiki))
---
+++ scripts/service_scripts/project_status_GL.py
@@ -0,0 +1,62 @@
+__author__ = 'erik'
+
+import sys
+import os
+import pywikibot
+from pywikibot.data.api import LoginManager
+import re
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + os.pardir + os.sep + os.pardir + os.sep )
+
+from tools.catscan import CatScan
+from tools.bots import CanonicalBot, SaveExecution
+from tools.little_helpers import load_password
+
+class GLStatus(CanonicalBot):
+    def __init__(self, wiki):
+        CanonicalBot.__init__(self, wiki)
+        self.botname = 'GLStatus'
+
+    def run(self):
+        all = self.petscan([''], pages=True)
+        fertig = self.petscan(['Fertig'], pages=True)
+        korrigiert = self.petscan(['Korrigiert'], pages=True)
+        unkorrigiert = self.petscan(['Unkorrigiert'], pages=True)
+        articles = self.petscan([])
+        if __debug__:
+            lemma = 'Benutzer:THEbotIT/' + self.botname
+        else:
+            lemma = 'Die Gartenlaube'
+        page = pywikibot.Page(self.wiki, lemma)
+        temp_text = page.text
+        composed_text = ''.join(['<!--new line: Liste wird von einem Bot aktuell gehalten.-->\n|-\n', '|', self.timestamp_start.strftime('%d.%m.%Y'),
+                         '|| ', str(all),
+                         ' || ', str(korrigiert), ' (', str(round((korrigiert/all)*100, 2)).replace('.', ','), ' %)',
+                         ' || ', str(fertig), ' (', str(round((fertig/all)*100, 2)).replace('.', ','), ' %)',
+                         ' || ', str(unkorrigiert), ' (', str(round((unkorrigiert/all)*100, 2)).replace('.', ','), ' %)',
+                         ' || ', str(articles), ' (', str(round((articles/17500)*100, 2)).replace('.', ','), ' %)', ' ||'])
+        temp_text = re.sub('<!--new line: Liste wird von einem Bot aktuell gehalten.-->', composed_text, temp_text)
+        page.text = temp_text
+        page.save('new dataset', botflag=True)
+
+    def petscan(self, categories, pages=False):
+        self.searcher = CatScan()
+        if pages:
+            self.searcher.add_namespace('Seite')
+        else:
+            self.searcher.add_namespace('Article')
+        self.searcher.set_search_depth(5)
+        self.searcher.add_positive_category('Die Gartenlaube')
+        for category in categories:
+            self.searcher.add_positive_category(category)
+        return len(self.searcher.run())
+
+if __name__ == "__main__":
+    with open('../password.pwd') as password_file:
+        password = load_password(password_file)
+        wiki = pywikibot.Site(code='de', fam='wikisource', user='THEbotIT')
+        login = LoginManager(site=wiki, password=password)
+        login.login()
+    bot = GLStatus(wiki)
+    with SaveExecution(bot):
+        bot.run()
