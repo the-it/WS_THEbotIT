519b646dcf0e8b23fe3e415d702dc84cd63d3000
Erik Sommer <ersotech@posteo.de>
2016-10-25 17:30:36 +0200

    RE_Scanner loading of pages

--- scripts/service_scripts/RE/TEST_re_scanner.py
+++ scripts/service_scripts/RE/TEST_re_scanner.py
@@ -1,15 +1,21 @@
 import re
 import time
+import operator
 from datetime import datetime, timedelta
 from tools.catscan import PetScan
 
 from tools.bots import CanonicalBot
 
+class ReScannerTask():
+    def __init__(self, wiki, debug):
+        self.reporter_page = None
+
+
 class ReScanner(CanonicalBot):
     def __init__(self, wiki, debug):
         CanonicalBot.__init__(self, wiki, debug)
         self.botname = 'ReScanner'
-        self.lemma_list = self.compile_lemma_list()
+        self.lemma_list = None
         self.new_data_model = datetime(year=2016, month=10, day=24, hour=23)
         self.timeout = timedelta(seconds=4)
 
@@ -22,6 +28,7 @@ class ReScanner(CanonicalBot):
             self.data = {}
 
     def compile_lemma_list(self):
+        self.logger.info('Compile the lemma list')
         searcher = PetScan()
         searcher.add_any_template('RE')
         searcher.add_any_template('REDaten')
@@ -29,12 +36,22 @@ class ReScanner(CanonicalBot):
             searcher.add_namespace(2)
         else:
             searcher.add_namespace(0)
-        return searcher.run()
+            searcher.add_positive_category('Fertig RE')
+            searcher.add_positive_category('Korrigiert RE')
+            searcher.set_logic_union()
+        self.logger.info(searcher)
+        raw_lemma_list = searcher.run()
+        # all items which wasn't process before
+        new_lemma_list = [x['title'] for x in raw_lemma_list if x['title'] not in list(self.data.keys())]
+        # before processed lemmas ordered by last process time
+        old_lemma_list = [x[0] for x in sorted(self.data.items(), key=operator.itemgetter(1))]
+        return new_lemma_list + old_lemma_list
 
     def run(self):
+        self.lemma_list = self.compile_lemma_list()
         for lemma in self.lemma_list:
+            self.logger.info('Process {}'.format(lemma))
+            self.data[lemma] = datetime.now().strftime('%Y%m%d%H%M%S')
+            time.sleep(1)
             if self._watchdog():
                 break
-            self.logger.info('{}, {}'.format(lemma['title'], lemma['touched']))
-            print(lemma)
-            time.sleep(1)
