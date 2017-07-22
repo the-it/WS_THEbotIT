e081c7b9098c5609e858d5d99142090919c46751
Erik Sommer <erotech@posteo.de>
2017-07-22 14:37:12 +0200

    make all changes together

--- scripts/online/170720_link_convert_search.py
+++ scripts/online/170720_link_convert_search.py
@@ -16,19 +16,19 @@ for idx, page in enumerate(searcher):
     print(idx, page)
     temp_text = page.text
     if re.search('\[http:\/\/www.ub.uni\-bielefeld.de', temp_text):
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/journalfranken\/journalfranken.htm UB Bielefeld\]', '{{Bielefeld|2096404}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/2006\/fontane_gedichte\/ UB Bielefeld\]', '{{Bielefeld|1682441}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/2005\/muehsam_erde\/ Digitale Drucke der Uni Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/journalfranken\/journalfranken.htm UB Bielefeld\]', '{{Bielefeld|2096404}}', temp_text)
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/2006\/fontane_gedichte\/ UB Bielefeld\]', '{{Bielefeld|1682441}}', temp_text)
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/2005\/muehsam_erde\/ Digitale Drucke der Uni Bielefeld\]',
                            '{{Bielefeld|1660345}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl/browse\/neuethalia\/11792.html Uni Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl/browse\/neuethalia\/11792.html Uni Bielefeld\]',
                            '{{Bielefeld|2300223_001}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/21792.html Uni Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/21792.html Uni Bielefeld\]',
                            '{{Bielefeld|2300223_002}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/11793.html Uni Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/11793.html Uni Bielefeld\]',
                            '{{Bielefeld|2300223_003}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/21793.html Uni Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/browse\/neuethalia\/21793.html Uni Bielefeld\]',
                            '{{Bielefeld|2300223_004}}', temp_text)
-        temp_text = re.sub('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/thalia\/thalia.htm UB Bielefeld\]',
+        temp_text, bla = re.subn('\[http:\/\/www.ub.uni\-bielefeld.de\/diglib\/aufkl\/thalia\/thalia.htm UB Bielefeld\]',
                            '{{Bielefeld|1944380}}', temp_text)
     if page.text != temp_text:
         page.text = temp_text
