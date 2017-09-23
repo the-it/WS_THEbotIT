7a8f0b21b82904a9df97b9aab49fcb9c14c652d3
Erik Sommer <erotech@posteo.de>
2017-09-23 14:30:05 +0200

    fix index

--- scripts/online/20170922_fix_index_pictures.py
+++ scripts/online/20170922_fix_index_pictures.py
@@ -12,10 +12,10 @@ from tools.catscan import PetScan
 regex_picture = re.compile('\|BILD=\[\[(.*?)\]\]')
 
 
-class PingOne(OneTimeBot):
+class FixIndex(OneTimeBot):
     def __init__(self, wiki, debug):
         OneTimeBot.__init__(self, wiki, debug)
-        self.bot_name = '20170922_Ping_One'
+        self.bot_name = '20170922_FixIndex'
         self.searcher = PetScan()
 
     def _search(self):
@@ -24,24 +24,25 @@ class PingOne(OneTimeBot):
 
     def run(self):
         lemma_list = self._search()
-        for lemma in lemma_list:
+        for idx, lemma in enumerate(lemma_list):
             page = Page(self.wiki, title='Index:{}'.format(lemma['title']))
-            self.logger.info(page)
+            self.logger.info('{}/{}:{}'.format(idx, len(lemma_list), page))
             match = regex_picture.search(page.text)
             if match:
                 self.logger.info(match.group(1))
                 if re.search('thumb', match.group(1)):
                     self.logger.info('nothing to do here')
-                    break
+                    continue
                 temp = '[[{}|thumb]]'.format(re.sub('\|\d{2,3}px', '', match.group(1)))
                 self.logger.info(temp)
                 temp_text = regex_picture.sub(temp, page.text)
                 page.text = temp_text
                 #page.save(botflag=True, summary='set thumb as parameter')
+        pass
 
 
 if __name__ == "__main__":
     wiki = Site(code='de', fam='wikisource', user='THEbotIT')
-    bot = PingOne(wiki=wiki, debug=True)
+    bot = FixIndex(wiki=wiki, debug=False)
     with SaveExecution(bot):
         bot.run()
