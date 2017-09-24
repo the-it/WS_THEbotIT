3fd1b25195fa380a22b3e27b31b13f3bb3f94dda
Erik Sommer <erotech@posteo.de>
2017-09-24 12:41:49 +0200

    more power for the starter

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 export DISPLAY=:0
 cd /home/pi/WS_THEbotIT/
-git pull
-pip3 install -r requirements.txt
+# fetch from the default remote, origin
+git fetch
+# reset your current branch (master) to origin's master
+git reset --hard origin/master
+sudo pip3 install -r requirements.txt
 cd scripts/
 python3 runner.py
