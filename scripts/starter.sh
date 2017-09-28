a2f49cc0590f41146cb0cbb7362db5a87815e89f
Erik Sommer <erotech@posteo.de>
2017-09-28 20:03:54 +0200

    test 3

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -6,4 +6,5 @@ git fetch
 # reset your current branch (master) to origin's master
 git reset --hard origin/master
 sudo pip3 install -r requirements.txt
-python3 scripts/runner.py
+cd scripts
+python3 runner.py
