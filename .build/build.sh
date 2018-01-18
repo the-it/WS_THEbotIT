1ff2e7eb55409ea4b482fa6f75d2e8598aaf5570
Erik Sommer <erotech@posteo.de>
2018-01-18 20:26:07 +0100

    repair build_script

--- .build/build.sh
+++ .build/build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-echo normal unit tests
-python -m unittest
-echo coverage
-coverage run tests.py
-echo pycodestyle
+echo "########### COVERAGE ###########"
+coverage run test/all_test.py
+echo "########## PYCODESTYLE #########"
 pep8
-echo flake8
+echo "############ FLAKE8 ############"
 flake8 --exit-zero
--- .build/post_script_code_climate.sh
+++ .build/post_script_code_climate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 export CC_TEST_REPORTER_ID=f3b7cf9220d85b6dde901a10d6f18747720138f87ed4f648bb7364d52f5310bb
-coverage run test/all_test.py
 coverage xml
 codecov --token=bb224da4-b91a-4080-b106-cb7bb5d84595
 ./cc-test-reporter format-coverage --output "coverage/codeclimate.$N.json"
