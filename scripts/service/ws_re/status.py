6fa0e8b806a38b208fe404f137001946e619685a
Erik Sommer <ersotech@posteo.de>
2018-04-11 23:42:37 +0200

    tested function for making a html color

--- scripts/service/ws_re/status.py
+++ scripts/service/ws_re/status.py
@@ -2,6 +2,7 @@ import re
 from datetime import datetime
 
 from pywikibot import Page, Site
+from tools import make_html_color
 from tools.catscan import PetScan
 from tools.bots import CanonicalBot
 
@@ -20,30 +21,19 @@ class ReStatus(CanonicalBot):
         self.history(fertig, korrigiert, unkorrigiert)
         return True
 
-    @staticmethod
-    def make_color(min_value, max_value, value):
-        if value > min_value:
-            if value < max_value:
-                color = (1 - (value - min_value) / (max_value - min_value)) * 255
-            else:
-                color = 0
-        else:
-            color = 255
-        return str(hex(int(color)))[2:].zfill(2)
-
     def user_page_the_it(self, korrigiert):
         status_string = []
 
-        color = self.make_color(20e6, 22e6, korrigiert[0])
+        color = make_html_color(20e6, 22e6, korrigiert[0])
         status_string.append('<span style="background:#FF{}{}">{}</span>'.format(color, color, korrigiert[0]))
-        color = self.make_color(5.0e3, 5.25e3, korrigiert[1])
+        color = make_html_color(5.0e3, 5.25e3, korrigiert[1])
         status_string.append('<span style="background:#FF{}{}">{}</span>'.format(color, color, korrigiert[1]))
 
         list_of_lemmas = self.petscan(['RE:Teilkorrigiert', 'RE:Korrigiert'], ['RE:Unkorrigiert', 'RE:Unvollständig'])
         date_page = Page(self.wiki, list_of_lemmas[0]['title'])
         date_of_first = str(date_page.oldest_revision.timestamp)[0:10]
         gap = datetime.now() - datetime.strptime(date_of_first, '%Y-%m-%d')
-        color = self.make_color(3 * 365, 3.5 * 365, gap.days)
+        color = make_html_color(3 * 365, 3.5 * 365, gap.days)
         status_string.append('<span style="background:#FF{}{}">{}</span>'.format(color, color, date_of_first))
 
         user_page = Page(self.wiki, 'Benutzer:THE IT/Werkstatt')
--- tools/__init__.py
+++ tools/__init__.py
@@ -2,4 +2,25 @@ class ToolException(Exception):
     pass
 
 
-__all__ = ['ToolException', 'catscan', 'template_handler', 'abbyy_xml', 'bots', 'date_conversion']
+__all__ = ['ToolException', 'catscan', 'template_handler', 'abbyy_xml', 'bots', 'date_conversion',
+           'make_html_color']
+
+
+def make_html_color(min_value, max_value, value):
+    if max_value > min_value:
+        if value > min_value:
+            if value < max_value:
+                color = (1 - (value - min_value) / (max_value - min_value)) * 255
+            else:
+                color = 0
+        else:
+            color = 255
+    else:
+        if value > max_value:
+            if value < min_value:
+                color = ((value - max_value) / (min_value - max_value)) * 255
+            else:
+                color = 255
+        else:
+            color = 0
+    return str(hex(round(color)))[2:].zfill(2).upper()
