ed6bde7355bb63cfb74b42eba6f9b8b578efaf55
Erik Sommer <ersotech@posteo.de>
2018-04-12 00:13:11 +0200

    simplify make html color

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,20 +7,9 @@ __all__ = ['ToolException', 'catscan', 'template_handler', 'abbyy_xml', 'bots', 
 
 
 def make_html_color(min_value, max_value, value):
-    if max_value > min_value:
-        if value > min_value:
-            if value < max_value:
-                color = (1 - (value - min_value) / (max_value - min_value)) * 255
-            else:
-                color = 0
-        else:
-            color = 255
+    if max_value >= min_value:
+        color = (1 - (value - min_value) / (max_value - min_value)) * 255
     else:
-        if value > max_value:
-            if value < min_value:
-                color = ((value - max_value) / (min_value - max_value)) * 255
-            else:
-                color = 255
-        else:
-            color = 0
+        color = ((value - max_value) / (min_value - max_value)) * 255
+    color = max(0, min(255, color))
     return str(hex(round(color)))[2:].zfill(2).upper()
