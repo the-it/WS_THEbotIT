2d72105b363a3f2b4f5978e07a6110b62eda42db
Erik Sommer <erotech@posteo.de>
2018-04-24 14:38:52 +0200

    some bugs fixed ... should work now

--- test/unit/scripts/service/little_tasks/test_hilbert_timer.py
+++ test/unit/scripts/service/little_tasks/test_hilbert_timer.py
@@ -13,7 +13,31 @@ class TestHilbertTimer(TestCase):
         with mock.patch("scripts.service.little_tasks.hilbert_timer.PetScan.run",
                         autospec=PetScan) as searcher_mock:
             searcher_mock.return_value = [1,2,3]
-            with mock.patch("scripts.service.little_tasks.hilbert_timer.HilbertTimer.get_days_to_end_of_2018",
-                            return_value=6):
-                self.assertEqual("<!--Hilbert--> Hilbert <span style=\"background:#B64900\" > 0.500</span> <!--Hilbert -->",
+            with mock.patch(
+                    "scripts.service.little_tasks.hilbert_timer.HilbertTimer.get_days_to_end_of_2018",
+                    return_value=6):
+                self.assertEqual(
+                    "<!--Hilbert--> Hilbert <span style=\"background:#49B600\" > 0.500</span> <!--Hilbert-->",
                                  HilbertTimer.replace_in_page("<!--Hilbert-->dummy<!--Hilbert-->"))
+
+    def test_green(self):
+        with mock.patch("scripts.service.little_tasks.hilbert_timer.PetScan.run",
+                        autospec=PetScan) as searcher_mock:
+            searcher_mock.return_value = [1, 2, 3]
+            with mock.patch(
+                    "scripts.service.little_tasks.hilbert_timer.HilbertTimer.get_days_to_end_of_2018",
+                    return_value=10):
+                self.assertEqual(
+                    "<!--Hilbert--> Hilbert <span style=\"background:#00FF00\" > 0.300</span> <!--Hilbert-->",
+                    HilbertTimer.replace_in_page("<!--Hilbert-->dummy<!--Hilbert-->"))
+
+    def test_red(self):
+        with mock.patch("scripts.service.little_tasks.hilbert_timer.PetScan.run",
+                        autospec=PetScan) as searcher_mock:
+            searcher_mock.return_value = [1, 2, 3]
+            with mock.patch(
+                    "scripts.service.little_tasks.hilbert_timer.HilbertTimer.get_days_to_end_of_2018",
+                    return_value=3):
+                self.assertEqual(
+                    "<!--Hilbert--> Hilbert <span style=\"background:#FF0000\" > 1.000</span> <!--Hilbert-->",
+                    HilbertTimer.replace_in_page("<!--Hilbert-->dummy<!--Hilbert-->"))
