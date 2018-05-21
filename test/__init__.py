e6771b22c16c717c3ed4b493c908396b44100a41
Erik Sommer <erotech@posteo.de>
2018-05-21 13:54:35 +0200

    don't test the same messages again and again

--- test/__init__.py
+++ test/__init__.py
@@ -5,4 +5,21 @@ from unittest import TestCase, skip
 import unittest.mock as mock
 from unittest.mock import patch
 
-__all__ = ["LogCapture", "TestCase", "ddt", "file_data", "mock", "patch", "responses", "skip"]
+__all__ = ["LogCapture", "TestCase",
+           "ddt", "file_data", "is_subtuple", "mock", "patch", "responses", "skip"]
+
+
+def is_subtuple(subtuple: tuple, supertuple: tuple) -> bool:
+    try:
+        old_index = supertuple.index(subtuple[0])
+    except ValueError:
+        return False
+    for member in subtuple[1:]:
+        try:
+            index = supertuple.index(member)
+        except ValueError:
+            return False
+        if not index == old_index + 1:
+            return False
+        old_index = index
+    return True
