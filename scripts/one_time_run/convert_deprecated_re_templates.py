c7e7b73e65c7afa7ae0b719756d7b44f30817dc7
Erik Sommer <erotech@posteo.de>
2018-06-08 16:27:55 +0200

    converters are ready

--- scripts/one_time_run/convert_deprecated_re_templates.py
+++ scripts/one_time_run/convert_deprecated_re_templates.py
@@ -9,9 +9,46 @@ class ConvertDeprecatedReTemplates(OneTimeBot):
     def convet_re_nachtrag(template):
         template_nachtrag = TemplateHandler(template)
         template_daten = TemplateHandler()
-        new_dict = template_nachtrag.get_parameterlist()
-        new_dict.append({"key": "NACHTRAG", "value": "ON"})
-        template_daten.update_parameters(new_dict)
+        new_list = template_nachtrag.get_parameterlist()
+        new_list.append({"key": "NACHTRAG", "value": "ON"})
+        template_daten.update_parameters(new_list)
+        template_daten.set_title("REDaten")
+
+        return template_daten.get_str()
+
+    @staticmethod
+    def _gemeinfrei_todesjahr(argument_list: list):
+        for idx, item in enumerate(argument_list):
+            if item["key"] == "GEMEINFREI":
+                year = item["value"]
+                del argument_list[idx]
+                if year:
+                    try:
+                        argument_list.insert(idx,
+                                             {"key": "TODESJAHR", "value": str(int(year) - 71)})
+                    except ValueError:
+                        raise ValueError("year is strange")
+                else:
+                    argument_list.insert(idx, {"key": "TODESJAHR", "value": "3333"})
+        return argument_list
+
+    def convet_re_platzhalter(self, template: str):
+        template_platzhalter = TemplateHandler(template)
+        template_daten = TemplateHandler()
+        new_list = template_platzhalter.get_parameterlist()
+        new_list = self._gemeinfrei_todesjahr(new_list)
+        template_daten.update_parameters(new_list)
+        template_daten.set_title("REDaten")
+
+        return template_daten.get_str()
+
+    def convet_re_nachtrag_platzhalter(self, template: str):
+        template_platzhalter = TemplateHandler(template)
+        template_daten = TemplateHandler()
+        new_list = template_platzhalter.get_parameterlist()
+        new_list.append({"key": "NACHTRAG", "value": "ON"})
+        new_list = self._gemeinfrei_todesjahr(new_list)
+        template_daten.update_parameters(new_list)
         template_daten.set_title("REDaten")
 
         return template_daten.get_str()
--- scripts/one_time_run/test_convert_deprecated_re_templates.py
+++ scripts/one_time_run/test_convert_deprecated_re_templates.py
@@ -37,5 +37,105 @@ class TestConvertDeprecatedReTemplates(TestCase):
 |ÜBERSCHRIFT=ON
 |NACHTRAG=ON
 }}"""
-        compare(post_text, ConvertDeprecatedReTemplates.convet_re_nachtrag(pre_text))
+        compare(post_text, ConvertDeprecatedReTemplates().convet_re_nachtrag(pre_text))
 
+    def test_convert_platzhalter(self):
+        pre_text = """{{RENachtrag/Platzhalter
+|BAND=S I
+|SPALTE_START=267
+|SPALTE_END=OFF
+|VORGÄNGER=Caecilius 42
+|NACHFOLGER=Caecilius 54a
+|SORTIERUNG=
+|KORREKTURSTAND=korrigiert
+|KEINE_SCHÖPFUNGSHÖHE=
+|GEMEINFREI=2071
+|WIKIPEDIA=
+|WIKISOURCE=
+|EXTSCAN_START={{REIA|S I|267}}
+|EXTSCAN_END=
+|ÜBERSCHRIFT=ON
+}}"""
+        post_text = """{{REDaten
+|BAND=S I
+|SPALTE_START=267
+|SPALTE_END=OFF
+|VORGÄNGER=Caecilius 42
+|NACHFOLGER=Caecilius 54a
+|SORTIERUNG=
+|KORREKTURSTAND=korrigiert
+|KEINE_SCHÖPFUNGSHÖHE=
+|TODESJAHR=2000
+|WIKIPEDIA=
+|WIKISOURCE=
+|EXTSCAN_START={{REIA|S I|267}}
+|EXTSCAN_END=
+|ÜBERSCHRIFT=ON
+}}"""
+        compare(post_text, ConvertDeprecatedReTemplates().convet_re_platzhalter(pre_text))
+
+    def test_convert_platzhalter_no_year(self):
+        pre_text = """{{RENachtrag/Platzhalter
+|GEMEINFREI=
+|ÜBERSCHRIFT=ON
+}}"""
+        post_text = """{{REDaten
+|TODESJAHR=3333
+|ÜBERSCHRIFT=ON
+}}"""
+        compare(post_text, ConvertDeprecatedReTemplates().convet_re_platzhalter(pre_text))
+
+    def test_convert_platzhalter_strange_year(self):
+        pre_text = """{{RENachtrag/Platzhalter
+|GEMEINFREI=abcd
+|ÜBERSCHRIFT=ON
+}}"""
+        with self.assertRaises(ValueError):
+            ConvertDeprecatedReTemplates().convet_re_platzhalter(pre_text)
+
+    def test_convert_platzhalter_death_year(self):
+        pre_text = """{{RENachtrag/Platzhalter
+|TODESJAHR=1988
+|ÜBERSCHRIFT=ON
+}}"""
+        post_text = """{{REDaten
+|TODESJAHR=1988
+|ÜBERSCHRIFT=ON
+}}"""
+        compare(post_text, ConvertDeprecatedReTemplates().convet_re_platzhalter(pre_text))
+
+    def test_convert_nachtrag_platzhalter(self):
+        pre_text = """{{RENachtrag/Platzhalter
+|BAND=S I
+|SPALTE_START=267
+|SPALTE_END=OFF
+|VORGÄNGER=Caecilius 42
+|NACHFOLGER=Caecilius 54a
+|SORTIERUNG=
+|KORREKTURSTAND=korrigiert
+|KEINE_SCHÖPFUNGSHÖHE=
+|GEMEINFREI=2071
+|WIKIPEDIA=
+|WIKISOURCE=
+|EXTSCAN_START={{REIA|S I|267}}
+|EXTSCAN_END=
+|ÜBERSCHRIFT=ON
+}}"""
+        post_text = """{{REDaten
+|BAND=S I
+|SPALTE_START=267
+|SPALTE_END=OFF
+|VORGÄNGER=Caecilius 42
+|NACHFOLGER=Caecilius 54a
+|SORTIERUNG=
+|KORREKTURSTAND=korrigiert
+|KEINE_SCHÖPFUNGSHÖHE=
+|TODESJAHR=2000
+|WIKIPEDIA=
+|WIKISOURCE=
+|EXTSCAN_START={{REIA|S I|267}}
+|EXTSCAN_END=
+|ÜBERSCHRIFT=ON
+|NACHTRAG=ON
+}}"""
+        compare(post_text, ConvertDeprecatedReTemplates().convet_re_nachtrag_platzhalter(pre_text))
