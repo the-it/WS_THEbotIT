90f567d96d51776c538bb28ef1c12b9e46b7d610
Erik Sommer <erotech@posteo.de>
2018-06-12 22:02:29 +0200

    do the rest

--- scripts/one_time_run/convert_deprecated_re_templates.py
+++ scripts/one_time_run/convert_deprecated_re_templates.py
@@ -106,8 +106,6 @@ class ConvertDeprecatedReTemplates(OneTimeBot):
             except (ReDatenException, ValueError):
                 error = traceback.format_exc().splitlines()[-1]
                 error_task.task(lemma["title"], error)
-            if idx > 600:
-                break
         error_task.finish_task()
         if self.search_pages():
             return False
