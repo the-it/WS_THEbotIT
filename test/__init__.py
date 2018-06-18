58ea22bf030c4fdfcc8e2f017bc3b0b12c1d9725
Erik Sommer <erotech@posteo.de>
2018-06-18 20:24:18 +0200

    fix codacy issues

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,10 @@
-from ddt import ddt, file_data
-import responses
-from testfixtures import LogCapture, StringComparison, compare
 from unittest import TestCase, skip, skipIf
 import unittest.mock as mock
 from unittest.mock import patch
+
+from ddt import ddt, file_data
+import responses
+from testfixtures import LogCapture, StringComparison, compare
 
 __all__ = ["LogCapture", "StringComparison", "TestCase",
            "compare", "ddt", "file_data", "mock", "patch", "responses", "skip", "skipIf"]
