af94510932f7734478aabd7e52d7b8f6e5f49e57
Erik Sommer <ersotech@posteo.de>
2018-07-02 18:48:21 +0200

    additional tests for percentage calculation

--- scripts/service/gl/test_status.py
+++ scripts/service/gl/test_status.py
@@ -16,3 +16,14 @@ bbb"""
         bot = GlStatus(None, False)
         bot.timestamp._start = datetime(year=2000, month=1, day=1)
         compare(result, bot.projektstand(given_file, 40000, 10000, 10000, 20000, 9250))
+
+    def test_to_percent(self):
+        test_array = (((0, 1), " (0,00 %)"),
+                      ((1, 4), " (25,00 %)"),
+                      ((1, 3), " (33,33 %)"),
+                      ((2, 3), " (66,67 %)"),
+                      ((3, 3), " (100,00 %)"))
+
+        bot = GlStatus(None, False)
+        for pair in test_array:
+            compare(pair[1], bot.to_percent(*pair[0]))
