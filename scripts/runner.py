e09dc871cd8712e02f230bd92f1cfbecf2736c4a
Erik Sommer <ersotech@posteo.de>
2018-09-17 17:29:07 +0200

    hilbert every day

--- scripts/runner.py
+++ scripts/runner.py
@@ -115,14 +115,14 @@ if __name__ == "__main__":  # pragma: no cover
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
     WS_WIKI = Site(code='de', fam='wikisource', user='THEbotIT')
     SCHEDULER = TheBotItScheduler(wiki=WS_WIKI, debug=False)
-    SCHEDULER.daily_bots = [AuthorList, ReScanner, GlCreateMagazine]
+    SCHEDULER.daily_bots = [AuthorList, ReScanner, GlCreateMagazine, HilbertTimer]
     SCHEDULER.weekly_bots = {0: [],  # monday
                              1: [],
                              2: [],
                              3: [],
                              4: [],
                              5: [],
-                             6: [ReStatus, HilbertTimer]}  # sunday
+                             6: [ReStatus]}  # sunday
     SCHEDULER.monthly_bots = {1: [GlStatus]}
     SCHEDULER.bots_on_last_day_of_month = []
     with SCHEDULER as bot:
