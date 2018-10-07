9bc43215c770d7ba164f51f12c6dd62a04321035
Erik Sommer <erotech@posteo.de>
2018-10-07 17:41:18 +0200

    better recognition of test startpoint (for usage in IDE)

--- test/all_tests.py
+++ test/all_tests.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 import sys
 import unittest
 
 LOADER = unittest.TestLoader()
-SUITE = LOADER.discover(start_dir=".")
+SUITE = LOADER.discover(start_dir=Path(__file__).parent.parent)
 RUNNER = unittest.TextTestRunner()
 sys.exit(not RUNNER.run(SUITE).wasSuccessful())
