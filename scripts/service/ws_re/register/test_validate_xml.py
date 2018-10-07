5ebb71ed02cac9ea3aa68d66ff0831df17e7cf6c
Erik Sommer <erotech@posteo.de>
2018-10-07 18:16:49 +0200

    identify all xml files to test

--- scripts/service/ws_re/register/test_validate_xml.py
+++ scripts/service/ws_re/register/test_validate_xml.py
@@ -3,19 +3,27 @@ from pathlib import Path
 from lxml import etree
 
 from test import *
+from tools import path_to_str
 
+
+def _get_all_xml_files():
+    path_to_register = Path(__file__).parent
+    return ("file1.xml", "file2.xml")
+
+
+@ddt
 class TestValidateXml(TestCase):
     def _get_register_xml(self):
         path_to_xsd = Path(__file__).parent.joinpath("register.xsd")
-        with open(str(path_to_xsd)) as xsd_file:
+        with open(path_to_str(path_to_xsd)) as xsd_file:
             return etree.XMLSchema(etree.parse(xsd_file))
 
     def setUp(self):
         self.register_xsd = self._get_register_xml()
 
-    def test_mock(self):
-        for file in ("file1.xml", "file2.xml"):
-            path_to_xml = Path(__file__).parent.joinpath(file)
-            with open(str(path_to_xml)) as xml:
-                doc_to_check = etree.parse(xml)
-                self.register_xsd.assertValid(doc_to_check)
+    @idata(_get_all_xml_files())
+    def test_register_xml(self, file):
+        path_to_xml = Path(__file__).parent.joinpath(file)
+        with open(path_to_str(path_to_xml)) as xml:
+            doc_to_check = etree.parse(xml)
+            self.register_xsd.assertValid(doc_to_check)
--- test/__init__.py
+++ test/__init__.py
@@ -2,9 +2,9 @@ from unittest import TestCase, skip, skipIf
 import unittest.mock as mock
 from unittest.mock import patch
 
-from ddt import ddt, file_data
+from ddt import ddt, file_data, idata
 import responses
 from testfixtures import LogCapture, StringComparison, compare
 
 __all__ = ["LogCapture", "StringComparison", "TestCase",
-           "compare", "ddt", "file_data", "mock", "patch", "responses", "skip", "skipIf"]
+           "compare", "ddt", "file_data", "idata", "mock", "patch", "responses", "skip", "skipIf"]
