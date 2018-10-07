221997f2b69694677032d597a759912ae0596911
Erik Sommer <erotech@posteo.de>
2018-10-07 21:07:28 +0200

    pimp pycodestyle

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 from typing import Union
 
+
 class ToolException(Exception):
     pass
 
@@ -18,6 +19,7 @@ def make_html_color(min_value, max_value, value):
         color = ((value - max_value) / (min_value - max_value)) * 255
     color = max(0, min(255, color))
     return str(hex(round(color)))[2:].zfill(2).upper()
+
 
 def path_to_str(path: Path) -> Union[str, Path]:
     if datetime.now() > datetime(year=2020, month=9, day=13):  # pragma: no cover
