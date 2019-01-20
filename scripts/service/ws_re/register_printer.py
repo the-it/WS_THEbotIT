65228931d2d55d7882ef13a5d23eac39297791e3
Erik Sommer <erotech@posteo.de>
2019-01-20 15:05:39 +0100

    quality

--- scripts/service/ws_re/register_printer.py
+++ scripts/service/ws_re/register_printer.py
@@ -18,6 +18,7 @@ class ReRegisterPrinter(CanonicalBot):
                 break
         return True
 
+
 if __name__ == "__main__":  # pragma: no cover
     WS_WIKI = Site(code='de', fam='wikisource', user='THEbotIT')
     with ReRegisterPrinter(wiki=WS_WIKI, debug=True, log_to_wiki=False) as bot:
