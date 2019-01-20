3e74ef1d1b9e0b8f50d72d47486a949489e933a7
Erik Sommer <erotech@posteo.de>
2019-01-20 14:59:22 +0100

    run daily

--- scripts/runner.py
+++ scripts/runner.py
@@ -15,6 +15,7 @@ from scripts.service.author_list import AuthorList
 from scripts.service.gl.create_magazine import GlCreateMagazine
 from scripts.service.gl.status import GlStatus
 from scripts.service.ws_re.importer import ReImporter
+from scripts.service.ws_re.register_printer import ReRegisterPrinter
 from scripts.service.ws_re.scanner import ReScanner
 from scripts.service.ws_re.status import ReStatus
 from tools import path_or_str
@@ -113,7 +114,7 @@ if __name__ == "__main__":  # pragma: no cover
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
     WS_WIKI = Site(code='de', fam='wikisource', user='THEbotIT')
     SCHEDULER = TheBotItScheduler(wiki=WS_WIKI, debug=False)
-    SCHEDULER.daily_bots = [AuthorList, ReScanner, GlCreateMagazine, ReImporter]
+    SCHEDULER.daily_bots = [AuthorList, ReScanner, GlCreateMagazine, ReImporter, ReRegisterPrinter]
     SCHEDULER.weekly_bots = {0: [],  # monday
                              1: [],
                              2: [],
