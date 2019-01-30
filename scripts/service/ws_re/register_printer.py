97b22ff631c6f6abd48864080b446e5f59bed0a5
Erik Sommer <erotech@posteo.de>
2019-01-30 21:37:49 +0100

    repair the register printer

--- scripts/service/ws_re/register_printer.py
+++ scripts/service/ws_re/register_printer.py
@@ -7,16 +7,16 @@ from tools.bots import CanonicalBot
 class ReRegisterPrinter(CanonicalBot):
     def task(self):  # pragma: no cover
         registers = Registers()
-        # for idx, register in enumerate(registers):
-        #     self.logger.info("Print Register {}.".format(register.volume.name))
-        #     self.save_if_changed(Page(self.wiki,
-        #                               "Paulys Realencyclopädie der classischen "
-        #                               "Altertumswissenschaft/Register/{}/new"
-        #                               .format(register.volume.name)),
-        #                          register.get_register_str(),
-        #                          "Register aktualisiert")
-        #     if idx > 6:
-        #         break
+        for idx, register in enumerate(registers):
+            self.logger.info("Print Register {}.".format(register.volume.name))
+            self.save_if_changed(Page(self.wiki,
+                                      "Paulys Realencyclopädie der classischen "
+                                      "Altertumswissenschaft/Register/{}/new"
+                                      .format(register.volume.name)),
+                                 register.get_register_str(),
+                                 "Register aktualisiert")
+            if idx > 6:
+                break
         for idx, register in enumerate(registers.alphabetic.values()):
             self.logger.info("Print Register {}.".format(register.start))
             self.save_if_changed(Page(self.wiki,
