e57fb45f386fdae2df77ae3ede73e2c8a3e7380c
Erik Sommer <erotech@posteo.de>
2019-01-31 22:21:55 +0100

    trailing whitespaces are removed by the wiki software, so the later text in the wiki wouldn't have such whitespaces.

--- tools/bots.py
+++ tools/bots.py
@@ -244,7 +244,7 @@ class OneTimeBot():
 
     @staticmethod
     def save_if_changed(page: Page, text: str, change_msg: str):
-        if text != page.text:
+        if text.rstrip() != page.text:
             page.text = text
             page.save(change_msg, botflag=True)
 
