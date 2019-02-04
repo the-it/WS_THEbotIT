7124d107e37afea7af10ab4801d35f10d9d3be25
Erik Sommer <erotech@posteo.de>
2019-02-04 19:06:49 +0100

    remove warnings - correct usage of bash variables

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 export DISPLAY=:0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BASE_DIR=$DIR/..
-echo $BASE_DIR
-cd $BASE_DIR
+BASE_DIR=${DIR}/..
+echo ${BASE_DIR}
+cd ${BASE_DIR}
 # fetch from the default remote, origin
 git fetch
 # reset your current branch (master) to origin's master
 git reset --hard origin/master
 sudo pip3 install -r requirements.txt
-export PYTHONPATH=$PYTHONPATH:$BASE_DIR
+export PYTHONPATH=${PYTHONPATH}:${BASE_DIR}
 python3 scripts/runner.py
