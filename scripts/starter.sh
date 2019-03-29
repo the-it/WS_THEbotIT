f6b06797489ebc70444373202079248373565715
Erik Sommer <erotech@posteo.de>
2019-03-29 23:48:12 +0100

    bug

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -8,6 +8,6 @@ cd ${BASE_DIR}
 git checkout master
 git pull
 git reset --hard master
-python3.7 -m pip install -r requirements.txt
+sudo python3.7 -m pip install -r requirements.txt
 export PYTHONPATH=${PYTHONPATH}:${BASE_DIR}
 python3.7 scripts/runner.py
