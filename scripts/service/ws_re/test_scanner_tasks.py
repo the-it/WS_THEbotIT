cf6160260b32b61a6a36bf563360f95dd9eca919
Erik Sommer <ersotech@posteo.de>
2019-04-15 17:23:44 +0200

    add title for mocking

--- scripts/service/ws_re/test_scanner_tasks.py
+++ scripts/service/ws_re/test_scanner_tasks.py
@@ -19,12 +19,14 @@ from tools.bots import WikiLogger
 class TaskTestCase(TestCase):
     # Todo: I don't like this, but it's working for the moment :-(, TestReScanner looks more elegant, but needs some investigation
     @mock.patch("scripts.service.ws_re.data_types.pywikibot.Page", autospec=pywikibot.Page)
-    @mock.patch("scripts.service.ws_re.data_types.pywikibot.Page.text",
-                new_callable=mock.PropertyMock)
-    def setUp(self, text_mock, page_mock):
+    @mock.patch("scripts.service.ws_re.data_types.pywikibot.Page.text", new_callable=mock.PropertyMock)
+    @mock.patch("scripts.service.ws_re.data_types.pywikibot.Page.title", new_callable=mock.Mock)
+    def setUp(self, title_mock, text_mock, page_mock):
         self.page_mock = page_mock
         self.text_mock = text_mock
+        self.title_mock = title_mock
         type(self.page_mock).text = self.text_mock
+        type(self.page_mock).title = self.title_mock
         self.logger = WikiLogger(bot_name="Test", start_time=datetime(2000, 1, 1),
                                  log_to_screen=False)
 
@@ -431,12 +433,11 @@ class TestSCANTask(TaskTestWithRegister):
                 compare("().git.checkout", repo_mock.mock_calls[7][0])
 
     def test_fetch_wikipedia_link(self):
-        self.text_mock.return_value = """[[Kategorie:RE:Verweisung]]
-{{REDaten
+        self.title_mock.return_value = "Lemma"
+        self.text_mock.return_value = """{{REDaten
 }}
 text.
-{{REAutor|OFF}}
-[[Kategorie:RE:Verweisung]]lala"""
+{{REAutor|OFF}}"""
         re_page = RePage(self.page_mock)
         with LogCapture():
             task = SCANTask(None, self.logger)
