2dbfeb6747d9f384f410f35d84c04d8b179d7fd6
Erik Sommer <ersotech@posteo.de>
2019-06-07 09:50:02 +0200

    correct download logic for Archive

--- scripts/service/ws_re/download/archive.py
+++ scripts/service/ws_re/download/archive.py
@@ -1,15 +1,15 @@
 import internetarchive
 
-from .base import base_path
+from scripts.service.ws_re.download.base import base_path
 
 _archives = {
-    "PaulySupp": "Pauly Supp.rar",
-    "PaulyWissowa5466": "Pauly-Wissowa_54-66.rar",
-    "PaulyWissowa4153_201412": "Pauly-Wissowa_41-53.rar",
-    "PaulyWissowa3140_201412": "Pauly-Wissowa_31-40.rar",
-    "PaulyWissowa2130": "Pauly-Wissowa_21-30.rar",
-    "PaulyWissowa1120": "Pauly-Wissowa_11-20.rar",
-    "PaulyWissowa110": "Pauly-Wissowa_1-10.rar",
+    "Pauly Supp.rar": "PaulySupp",
+    "Pauly-Wissowa_54-66.rar": "PaulyWissowa5466",
+    "Pauly-Wissowa_41-53.rar": "PaulyWissowa4153_201412",
+    "Pauly-Wissowa_31-40.rar": "PaulyWissowa3140_201412",
+    "Pauly-Wissowa_21-30.rar": "PaulyWissowa2130",
+    "Pauly-Wissowa_11-20.rar": "PaulyWissowa1120",
+    "Pauly-Wissowa_1-10.rar": "PaulyWissowa110",
              }
 
 
@@ -21,4 +21,13 @@ class Archive():
     def get_target(self):
         session = internetarchive.ArchiveSession()
         download = session.get_item(self.source).get_file(self.target)
-        download.download(base_path.joinpath("archives").joinpath(self.source))
+        download.download(str(base_path.joinpath("archives").joinpath(self.source)))
+
+if __name__ == "__main__":
+    Archive("Pauly-Wissowa_1-10.rar").get_target()
+    Archive("Pauly Supp.rar").get_target()
+    Archive("Pauly-Wissowa_54-66.rar").get_target()
+    Archive("Pauly-Wissowa_41-53.rar").get_target()
+    Archive("Pauly-Wissowa_31-40.rar").get_target()
+    Archive("Pauly-Wissowa_21-30.rar").get_target()
+    Archive("Pauly-Wissowa_11-20.rar").get_target()
--- scripts/service/ws_re/download/base.py
+++ scripts/service/ws_re/download/base.py
@@ -7,9 +7,11 @@ base_path = None
 if platform == "win32":
     base_path = Path("C:/RE")
 elif platform == "linux" or platform == "linux2":
-    base_path = Path("/usr/re")
+    base_path = Path("/home/erik/re")
 else:
     raise NotImplementedError("the used os ist not implemented yet, please specify the path for your os.")
 
 if not os.path.isdir(base_path):
     os.mkdir(base_path)
+
+
