0fece2f0713992ecbf4b98e9567c0ed7015e323a
Erik Sommer <ersotech@posteo.de>
2019-06-09 23:21:52 +0200

    finish mapping for PaulyWissowa1120

--- scripts/service/ws_re/download/mapping.py
+++ scripts/service/ws_re/download/mapping.py
@@ -1,4 +1,5 @@
 import re
+import shutil
 from os import listdir, symlink, makedirs
 from os.path import isfile, join
 from pathlib import Path
@@ -22,6 +23,16 @@ _MAPPINGS = {
     "V,2": {"source": ("PaulyWissowa110", "Demogenes Ephoroi"), "pages": ((1531, 90385, 90718),)},
     "VI,1": {"source": ("PaulyWissowa110", "Ephoros-Eutychos"), "pages": ((-1, 2, 386),)},
     "VI,2": {"source": ("PaulyWissowa110", "Euxantios-Fornaces"), "pages": ((1535, 2, 337),)},
+    "VII,1": {"source": ("PaulyWissowa1120", "Fornax-Glykon"), "pages": ((-1, 2, 370),)},
+    "VII,2": {"source": ("PaulyWissowa1120", "Glykyrrh-Helikeia"), "pages": ((1471, 2, 354),)},
+    "VIII,1": {"source": ("PaulyWissowa1120", "Helikon-Hestia"), "pages": ((-1, 2, 330),)},
+    "VIII,2": {"source": ("PaulyWissowa1120", "Hestiaia Hyagnis"), "pages": ((1311, 160002, 160331),)},
+    "IX,1": {"source": ("PaulyWissowa1120", "Hyaia Imperator"), "pages": ((-1, 170002, 170301),)},
+    "IX,2": {"source": ("PaulyWissowa1120", "Imperium Iugum"), "pages": ((1199, 180002, 180358),)},
+    "X,1": {"source": ("PaulyWissowa1120", "Iugurtha Ius Latii"), "pages": ((-1, 190002, 190322),)},
+    "X,2": {"source": ("PaulyWissowa1120", "Ius liberorum Katochos"), "pages": ((1279, 200002, 200318),)},
+    "XI,1": {"source": ("PaulyWissowa1120", "Katoikoi Komoedie"), "pages": ((-1, 210002, 210322),)},
+    "XI,2": {"source": ("PaulyWissowa1120", "Komogrammateus Kynegoi"), "pages": ((1279, 220002, 220314),)},
 }
 
 
@@ -51,12 +62,23 @@ class Mapping(DownloadTarget):
             print(f"Mapping {self.path_mapping} doesn't exists, produce it now ")
             self.get_source()
             _photo_mapping = self._get_raw_files_photo_mapping()
-            for page_mapping in _MAPPINGS[self.target]["pages"]:
-                offset = page_mapping[0]
-                makedirs(self.path_mapping, exist_ok=True)
-                for idx, page in enumerate(range(page_mapping[1], page_mapping[2] + 1)):
-                    symlink(Path(self.path_raw_files, _photo_mapping[page]),
-                            Path(self.path_mapping, f"{offset + (4*idx)}.tif"))
+            try:
+                for page_mapping in _MAPPINGS[self.target]["pages"]:
+                    offset = page_mapping[0]
+                    makedirs(self.path_mapping, exist_ok=True)
+                    for idx, page in enumerate(range(page_mapping[1], page_mapping[2] + 1)):
+                        try:
+                            symlink(Path(self.path_raw_files, _photo_mapping[page]),
+                                    Path(self.path_mapping, f"{offset + (4*idx)}.tif"))
+                        except KeyError as error:
+                            # known absent pages
+                            if error.args[0] in (170260, 170265, 170270, 170275, 170280, 170285,
+                                                 170290, 170291, 170292, 170295):
+                                continue
+                            raise error
+            except KeyError as error:
+                shutil.rmtree(self.path_mapping)
+                raise error
         else:
             print(f"Mapping {self.path_mapping} exists")
 
