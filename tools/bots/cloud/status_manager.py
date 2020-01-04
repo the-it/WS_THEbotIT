8a913ee62e017a12edcafb023c0a6d88ce85699d
Erik Sommer <ersotech@posteo.de>
2020-01-04 18:37:19 +0100

    creating unique ids ... kind of

--- tools/bots/cloud/status_manager.py
+++ tools/bots/cloud/status_manager.py
@@ -10,5 +10,6 @@ class StatusManager:
     def __init__(self, bot_name: str):
         self.dynamodb: dynamodb.DynamoDBServiceResource = boto3.resource('dynamodb', region_name='eu-central-1')
         self.manage_table = self.dynamodb.Table(self.MANAGE_TABLE)  # pylint: disable=no-member
-        self.current_run = Status(1, bot_name)
+        # self.manage_table.query(ScanIndexForward=False, Limit=1)
+        self.current_run = Status(self.manage_table.item_count, bot_name)
         self.manage_table.put_item(Item=self.current_run.to_dict())  # type: ignore
--- tools/bots/cloud/test_status_manager.py
+++ tools/bots/cloud/test_status_manager.py
@@ -12,24 +12,24 @@ StatusManager.MANAGE_TABLE = "wiki_bots_manage_table_tst"
 class TestStatusManager(TestCloudBase):
     def test_init(self):
         status_manager = StatusManager("TestBot")
-        compare(1, status_manager.current_run.id)
-        compare({"id": 1,
+        compare(0, status_manager.current_run.id)
+        compare({"id": 0,
                  "bot_name": "TestBot",
                  "finish": mock.ANY,
                  "success": mock.ANY,
                  "start_time": mock.ANY,
                  "finish_time": mock.ANY,
                  "output": mock.ANY},
-                self.manage_table.get_item(Key={"id": 1})["Item"])
+                self.manage_table.get_item(Key={"id": 0})["Item"])
 
 
     def test_unique_id(self):
         status_manager = StatusManager("TestBot")
-        compare(1, status_manager.current_run.id)
+        compare(0, status_manager.current_run.id)
+        compare(0, self.manage_table.get_item(Key={"id": 0})["Item"]["id"])
+        status_manager_1 = StatusManager("TestBot1")
+        compare(1, status_manager_1.current_run.id)
         compare(1, self.manage_table.get_item(Key={"id": 1})["Item"]["id"])
-        # status_manager_2 = StatusManager("TestBot2")
-        # compare(2, status_manager_2.current_run.id)
-        # compare(2, self.manage_table.get_item(Key={"id": 2})["Item"]["id"])
-
-    def test_3(self):
-        pass
+        status_manager_2 = StatusManager("TestBot")
+        compare(2, status_manager_2.current_run.id)
+        compare(2, self.manage_table.get_item(Key={"id": 2})["Item"]["id"])
