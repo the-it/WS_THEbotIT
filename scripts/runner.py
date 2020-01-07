ad4b28f2220f3dea374e1f79de93e7b40e5dab12
Erik Sommer <ersotech@posteo.de>
2020-01-07 23:22:46 +0100

    reduce runtime of ReScanner

--- scripts/runner.py
+++ scripts/runner.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     SCHEDULER = BotScheduler(wiki=WS_WIKI, debug=False)
     SCHEDULER.daily_bots = [AuthorList, ReRegisterPrinter]
-    SCHEDULER.weekly_bots = {0: [ReScanner],  # monday
-                             1: [ReScanner],
+    SCHEDULER.weekly_bots = {0: [],  # monday
+                             1: [],
                              2: [ReScanner],
-                             3: [ReScanner],
-                             4: [ReScanner],
-                             5: [ReScanner],
-                             6: [ReScanner, ReStatus, GlCreateMagazine, ReScanner]}  # sunday
+                             3: [],
+                             4: [],
+                             5: [],
+                             6: [ReScanner, ReStatus, GlCreateMagazine]}  # sunday
     SCHEDULER.monthly_bots = {1: [GlStatus]}
     SCHEDULER.bots_on_last_day_of_month = []
     with SCHEDULER as bot:
