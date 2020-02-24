b5f9f36daf680eb1873558dcd853b99ccf85d1a7
Erik Sommer <ersotech@posteo.de>
2020-02-24 20:06:55 +0100

    change path to stored re data

--- scripts/service/ws_re/download/base.py
+++ scripts/service/ws_re/download/base.py
@@ -2,7 +2,7 @@ import os
 from abc import ABC, abstractmethod
 from pathlib import Path
 
-BASE_PATH = Path.home().joinpath("re")
+BASE_PATH = Path("/mnt/temp_erik/re")
 
 if not os.path.isdir(BASE_PATH):
     os.mkdir(BASE_PATH)
