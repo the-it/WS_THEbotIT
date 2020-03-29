b0bdbc165ee50c6c96198fa799c2f6208ae8a720
Erik Sommer <ersotech@posteo.de>
2020-03-29 21:10:51 +0200

    update of labels, descriptions and sitelinks

--- scripts/service/ws_re/scanner/tasks/wikidata.py
+++ scripts/service/ws_re/scanner/tasks/wikidata.py
@@ -1,3 +1,6 @@
+from typing import Dict, Callable
+
+import dictdiffer
 import pywikibot
 
 from scripts.service.ws_re.scanner import ReScannerTask
@@ -5,21 +8,73 @@ from tools.bots.pi import WikiLogger
 
 
 class DATATask(ReScannerTask):
+    _languages = ("de", "en", "fr", "nl")
+
     def __init__(self, wiki: pywikibot.Site, logger: WikiLogger, debug: bool = True):
         ReScannerTask.__init__(self, wiki, logger, debug)
         self.wikidata: pywikibot.site.DataSite = pywikibot.Site(code="wikidata", fam="wikidata", user="THEbotIT")
 
     def task(self):
         try:
+            # edit existing wikidata item
             data_item: pywikibot.ItemPage = self.re_page.page.data_item()
+            self._update_non_properties(data_item)
         except pywikibot.NoPage:
+            # create a new one from scratch
             data_item: pywikibot.ItemPage = self.wikidata.createNewItemFromPage(page=self.re_page.page)
-        self._p31(data_item)
 
-        #data_item.save()
+        self._get_property_functions()
+        # data_item.editEntity(new_values)
 
-    def _p31(self, data_item: pywikibot.ItemPage):
-        claim =  pywikibot.Claim(self.wikidata, 'P31')
+        # data_item.save()
+
+    def _update_non_properties(self, item: pywikibot.ItemPage):
+        non_properties = self._non_properties
+        if self._labels_and_sitelinks_has_changed(item, non_properties):
+            item.editEntity(non_properties)
+
+    @property
+    def _non_properties(self) -> Dict:
+        non_properties = {"labels": {
+            "de": {"language": "de", "value": f"{self.re_page.lemma_without_prefix} (Pauly-Wissowa)"},
+            "en": {"language": "en", "value": f"{self.re_page.lemma_without_prefix} (Pauly-Wissowa)"},
+            "fr": {"language": "fr", "value": f"{self.re_page.lemma_without_prefix} (Pauly-Wissowa)"},
+            "nl": {"language": "nl", "value": f"{self.re_page.lemma_without_prefix} (Pauly-Wissowa)"},
+        },
+            "descriptions": {
+                "de": {"language": "de", "value": "Enzyklopädischer Artikel in Wikisource"},
+                "en": {"language": "en", "value": "wikisource encyclopedic article"},
+                "fr": {"language": "fr", "value": "article d'encyclopédie de wikisource"},
+                "nl": {"language": "nl", "value": "wikisource encyclopedisch artikel"},
+            },
+            "sitelinks": [{'site': 'dewikisource', 'title': self.re_page.page.title(), "badges": []}]
+        }
+        return non_properties
+
+    def _labels_and_sitelinks_has_changed(self, item: pywikibot.ItemPage, new_non_properties: Dict) -> bool:
+        old_non_properties = item.toJSON()
+        # claims are not relevant here
+        del old_non_properties["claims"]
+        # reformat sitelinks (toJSON has different format then editEntity accepts)
+        old_non_properties["sitelinks"] = [sitelink for sitelink in old_non_properties["sitelinks"].values()]
+        # remove all languages, that are not set by this bot
+        for labels_or_descriptions in ("labels", "descriptions"):
+            old_non_properties[labels_or_descriptions] = {key: value
+                                                          for (key, value)
+                                                          in old_non_properties[labels_or_descriptions].items()
+                                                          if key in self._languages}
+        return bool(tuple(dictdiffer.diff(new_non_properties, old_non_properties)))
+
+    def _get_property_functions(self) -> Dict[str, Callable]:
+        property_functions = {}
+        for item in dir(self):
+            if "_p" in item:
+                property_functions[item[1:].upper()] = getattr(self, item)
+        return property_functions
+
+    # from here on all functions are related to one specific property
+    def _p31(self) -> pywikibot.Claim:
+        claim = pywikibot.Claim(self.wikidata, 'P31')
         target = pywikibot.ItemPage(self.wikidata, "Q13433827")
         claim.setTarget(target)
-        data_item.addClaim(claim, summary="Adding claim P31")
+        return claim
