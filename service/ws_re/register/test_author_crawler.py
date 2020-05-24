10a34257a4a64b11227dc428a1c1b31f05c3d231
Erik Sommer <ersotech@posteo.de>
2020-05-24 12:16:29 +0200

    add lemma attribute to register author

--- service/ws_re/register/test_author_crawler.py
+++ service/ws_re/register/test_author_crawler.py
@@ -108,21 +108,25 @@ class TestAuthorCrawler(TestCase):
                   "Zwicker.": "Johannes Zwicker"}
         compare(expect, self.crawler.get_mapping(test_str))
 
-    def test_extract_author_name(self):
-        compare(("Klaus", "Alpers"), self.crawler._extract_author_name("|Alpers, Klaus"))
-        compare(("Franz", "Altheim"), self.crawler._extract_author_name("Altheim, [Franz]"))
-        compare(("Wolfgang", "Aly"), self.crawler._extract_author_name("|[[Wolfgang Aly|Aly, Wolf[gang]]]"))
-        compare(("Walter", "Amelung"),
-                self.crawler._extract_author_name("|'''[[Walter Amelung|Amelung, [Walter]]]'''"))
-        compare(("Hermann", "Abert"), self.crawler._extract_author_name("|'''[[Hermann Abert|Abert, [Hermann]]]"))
-        compare(("Martin", "Bang"), self.crawler._extract_author_name("Bang, [Martin]{{Anker | B}}"))
-        compare(("Johannes", "Zwicker"), self.crawler._extract_author_name("Zwicker, Joh[annes = Hanns]"))
-        compare(("Friedrich Walter", "Lenz"),
-                self.crawler._extract_author_name("Lenz, Friedrich [Walter] = (Levy, [Friedrich Walter])"))
-        compare(("Eduard", "Thraemer"),
-                self.crawler._extract_author_name("'''[[Eduard Thraemer|Thraemer [= Thrämer], Eduard]]'''"))
-        compare(("", "Buren s. Van Buren"), self.crawler._extract_author_name("Buren s. Van Buren"))
-        compare(("Karl", "Praechter"), self.crawler._extract_author_name("Praechter, K[arl]<nowiki></nowiki>"))
+    def test_extract_author_infos(self):
+        compare(("Klaus", "Alpers", None), self.crawler._extract_author_infos("|Alpers, Klaus"))
+        compare(("Franz", "Altheim", None), self.crawler._extract_author_infos("Altheim, [Franz]"))
+        compare(("Wolfgang", "Aly", "Wolfgang Aly"), self.crawler._extract_author_infos("|[[Wolfgang Aly|Aly, Wolf[gang]]]"))
+        compare(("Walter", "Amelung", "Walter Amelung"),
+                self.crawler._extract_author_infos("|'''[[Walter Amelung|Amelung, [Walter]]]'''"))
+        compare(("Hermann", "Abert", "Hermann Abert"), self.crawler._extract_author_infos("|'''[[Hermann Abert|Abert, [Hermann]]]"))
+        compare(("Martin", "Bang", None), self.crawler._extract_author_infos("Bang, [Martin]{{Anker | B}}"))
+        compare(("Johannes", "Zwicker", None), self.crawler._extract_author_infos("Zwicker, Joh[annes = Hanns]"))
+        compare(("Friedrich Walter", "Lenz", None),
+                self.crawler._extract_author_infos("Lenz, Friedrich [Walter] = (Levy, [Friedrich Walter])"))
+        compare(("Eduard", "Thraemer", "Eduard Thraemer"),
+                self.crawler._extract_author_infos("'''[[Eduard Thraemer|Thraemer [= Thrämer], Eduard]]'''"))
+        compare(("", "Buren s. Van Buren", None), self.crawler._extract_author_infos("Buren s. Van Buren"))
+        compare(("Karl", "Praechter", None), self.crawler._extract_author_infos("Praechter, K[arl]<nowiki></nowiki>"))
+        compare(("Richard", "Wagner", "Richard Wagner (Philologe)"), self.crawler._extract_author_infos("'''[[Richard Wagner (Philologe)|Wagner, [Richard]]]'''"))
+        compare(("Anton", "Baumstark", "Anton Baumstark junior"),self.crawler._extract_author_infos("'''[[Anton Baumstark junior|Baumstark, [Anton (jr.)]]]'''"))
+
+
 
     def test_extract_years(self):
         compare((1906, 1988), self.crawler._extract_years("1906–1988"))
@@ -159,7 +163,7 @@ Als Kontrollgrundlage dienen in erster Linie die Angaben im Werk selbst:
 |XIX,2
 |[[w:Kenneth Morgan Abbott|Wikipedia]]
 |-
-|Abel, Karlhans
+|[[Karlhans Abel|Abel, Karlhans]]
 |1919–1998
 |X A, S XII, S XIV
 |[[w:Karlhans Abel|Wikipedia]]
@@ -222,7 +226,7 @@ Als Kontrollgrundlage dienen in erster Linie die Angaben im Werk selbst:
     def test_get_complete_authors(self):
         author_mapping = self.crawler.get_authors(self.author_table)
         expect = {"Kenneth Morgan Abbott": {"birth": 1906, "death": 1988, "first_name": "Kenneth Morgan", "last_name": "Abbott"},
-                  "Karlhans Abel": {"birth": 1919, "death": 1998, "first_name": "Karlhans", "last_name": "Abel"},
+                  "Karlhans Abel": {"birth": 1919, "death": 1998, "first_name": "Karlhans", "last_name": "Abel", "lemma": "Karlhans Abel"},
                   "Walther Abel": {"birth": 1906, "death": 1987, "first_name": "Walther", "last_name": "Abel"},
                   "Wolf ?": {"last_name": "Wolf ?"},
                   "Johannes Zwicker": {"birth": 1881, "death": 1969, "first_name": "Johannes", "last_name": "Zwicker"}}
