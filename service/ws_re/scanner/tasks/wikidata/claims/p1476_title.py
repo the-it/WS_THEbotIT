f3c9e82e1f7948a51060b4ce29c0cc5c9d31a459
Erik Sommer <ersotech@posteo.de>
2020-06-01 21:33:50 +0200

    [remove title] it isn't possible to set the correct language for the title

--- service/ws_re/scanner/tasks/wikidata/claims/p1476_title.py
+++ service/ws_re/scanner/tasks/wikidata/claims/p1476_title.py
@@ -1,17 +1,13 @@
 from typing import List
 
-from service.ws_re.scanner.tasks.wikidata.claims.claim_factory import ClaimFactory
-from service.ws_re.scanner.tasks.wikidata.claims._base import SnakParameter
 from service.ws_re.scanner.tasks.wikidata.claims._typing import JsonClaimDict
+from service.ws_re.scanner.tasks.wikidata.claims.claim_factory import ClaimFactory
 
 
 class P1476Title(ClaimFactory):
     """
-    Returns the Claim **title** -> **<title for the RE lemma>**
+    Empties the title claim, was kind of a bad idea
     """
 
     def _get_claim_json(self) -> List[JsonClaimDict]:
-        snak_parameter = SnakParameter(property_str=self.get_property_string(),
-                                       target_type="monolingualtext",
-                                       target=self.re_page.lemma_without_prefix)
-        return [self.create_claim_json(snak_parameter)]
+        return []
