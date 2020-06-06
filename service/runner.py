d8d9586928f30923af95e73d98cb53cc7174c819
Erik Sommer <ersotech@posteo.de>
2020-06-06 15:34:50 +0200

    [hotfix] update Registers just ones every week

--- service/runner.py
+++ service/runner.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())  # type: ignore
     WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     SCHEDULER = BotScheduler(wiki=WS_WIKI, debug=False)
-    SCHEDULER.daily_bots = [AuthorList, ReScanner, ReRegisterPrinter]
-    SCHEDULER.weekly_bots = {0: [],  # monday
+    SCHEDULER.daily_bots = [AuthorList, ReScanner]
+    SCHEDULER.weekly_bots = {0: [ReRegisterPrinter],  # monday
                              1: [],
                              2: [],
                              3: [],
