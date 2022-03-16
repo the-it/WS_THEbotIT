23e629a7f2194e1d9cc083ebe6e35956f4cfc0f1
Erik Sommer <ersotech@posteo.de>
2022-03-16 16:59:58 +0100

    parsing short description from wiki source files

--- service/ws_re/scanner/tasks/add_short_description.py
+++ service/ws_re/scanner/tasks/add_short_description.py
@@ -1,4 +1,4 @@
-import datetime
+import re
 from typing import Dict
 
 import pywikibot
@@ -13,9 +13,24 @@ class KURZTask(ReScannerTask):
     def __init__(self, wiki: pywikibot.Site, logger: WikiLogger, debug: bool = True):
         super().__init__(wiki, logger, debug)
         self.short_description_lookup: Dict[str, str] = {}
-        source_short_description = pywikibot.Page(self.wiki, "Wikisource:RE-Werkstatt/Kurzbeschreibung/a")
 
-    def _load_source_short_description(self, source_text: str):
+    def _get_short_description_text_from_source(self, starting_letter: str) -> str:
+        text = pywikibot.Page(self.wiki, f"Wikisource:RE-Werkstatt/Kurzbeschreibung/{starting_letter}").text
+        if not text:
+            raise ValueError(f"There is no contenten on Wikisource:RE-Werkstatt/Kurzbeschreibung/{starting_letter}")
+        return text
+
+    @staticmethod
+    def _load_source_short_description(source_text: str) -> Dict[str,str]:
+        new_lookup_dict: Dict[str, str] = {}
+        # splitting up by line and processing only the relevant lines from the table
+        for line in source_text.strip().splitlines()[4::2]:
+            # first group is the lemma, second is the description
+            match = re.search(r"\[\[RE:([^\]]*?)\]\]\|\|(.*)", line)
+            # don't process if there is an invalid description
+            if match.group(2) != "(-)":
+                new_lookup_dict[match.group(1)] = match.group(2)
+        return new_lookup_dict
 
     def task(self):
         for re_article in self.re_page:
--- service/ws_re/scanner/tasks/test_add_short_description.py
+++ service/ws_re/scanner/tasks/test_add_short_description.py
@@ -5,15 +5,16 @@ from service.ws_re.scanner.tasks.test_base_task import TaskTestCase
 from service.ws_re.template.re_page import RePage
 
 class TestKURZTaskProcessSource(TaskTestCase):
-    def test_1st(self):
-        source_short_text = """{{REDaten
-|BAND=S I
-|KEINE_SCHÖPFUNGSHÖHE=ON
-|TODESJAHR=1950
-}}
-bla
-{{REAutor|Stein.}}"""
-        re_page = RePage(self.page_mock)
-        task = PDKSTask(None, self.logger)
-        compare({"success": True, "changed": True}, task.run(re_page))
-        compare("", re_page[0]["TODESJAHR"].value)
+    def test_load_short_descriptions_from_text(self):
+        source_short_text = """{|class="wikitable"
+|-
+!Artikel!!Kurzbeschreibung
+|-
+|[[RE:Aachen]]||deutsche Stadt = Aquae
+|-
+|[[RE:Aal]]||Zoologisch
+|-
+|[[RE:No real description]]||(-)
+|}"""
+        short_text_lookup = KURZTask._load_source_short_description(source_short_text)
+        compare(short_text_lookup, {"Aachen": "deutsche Stadt = Aquae", "Aal": "Zoologisch"})
