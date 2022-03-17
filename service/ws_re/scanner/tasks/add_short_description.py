8c241d080c3ef44e85c30f727ed4da545c4d7433
Erik Sommer <ersotech@posteo.de>
2022-03-17 16:42:37 +0100

    finish add short description feature for the moment

--- service/ws_re/scanner/tasks/add_short_description.py
+++ service/ws_re/scanner/tasks/add_short_description.py
@@ -5,42 +5,52 @@ import pywikibot
 
 from service.ws_re.register.registers import RE_ALPHABET
 from service.ws_re.scanner.tasks.base_task import ReScannerTask
-from service.ws_re.template.article import Article
 from tools.bots.pi import WikiLogger
 
 
 class KURZTask(ReScannerTask):
+    MAINTENANCE_CAT = "[[Kategorie:RE:Kurztext überprüfen]]"
+    SHORT_TEXT_URL = "Wikisource:RE-Werkstatt/Kurzbeschreibung/"
+
     def __init__(self, wiki: pywikibot.Site, logger: WikiLogger, debug: bool = True):
         super().__init__(wiki, logger, debug)
-        self.short_description_lookup: Dict[str, str] = {}
+        self.short_description_lookup: Dict[str, str] = self._load_short_descriptions()
 
     def _get_short_description_text_from_source(self, starting_letter: str) -> str:
-        text = pywikibot.Page(self.wiki, f"Wikisource:RE-Werkstatt/Kurzbeschreibung/{starting_letter}").text
+        text: str = pywikibot.Page(self.wiki, f"{self.SHORT_TEXT_URL}{starting_letter}").text
         if not text:
-            raise ValueError(f"There is no contenten on Wikisource:RE-Werkstatt/Kurzbeschreibung/{starting_letter}")
+            raise ValueError(f"There is no content on {self.SHORT_TEXT_URL}{starting_letter}")
         return text
 
+    def _load_short_descriptions(self):
+        complete_descriptions_dict = {}
+        for letter in RE_ALPHABET:
+            try:
+                complete_descriptions_dict.update(
+                    self._parse_short_description(self._get_short_description_text_from_source(letter)))
+            except ValueError:
+                self.logger.error(f"Couldn't load {self.SHORT_TEXT_URL}{letter}.")
+        return complete_descriptions_dict
+
     @staticmethod
-    def _load_source_short_description(source_text: str) -> Dict[str,str]:
+    def _parse_short_description(source_text: str) -> Dict[str, str]:
         new_lookup_dict: Dict[str, str] = {}
         # splitting up by line and processing only the relevant lines from the table
         for line in source_text.strip().splitlines()[4::2]:
             # first group is the lemma, second is the description
             match = re.search(r"\[\[RE:([^\]]*?)\]\]\|\|(.*)", line)
             # don't process if there is an invalid description
-            if match.group(2) != "(-)":
-                new_lookup_dict[match.group(1)] = match.group(2)
+            if match:
+                if match.group(2) != "(-)":
+                    new_lookup_dict[match.group(1)] = match.group(2)
         return new_lookup_dict
 
     def task(self):
-        for re_article in self.re_page:
-            if isinstance(re_article, Article):
-                if re_article["KEINE_SCHÖPFUNGSHÖHE"].value:
-                    if todesjahr := re_article["TODESJAHR"].value:
-                        if int(todesjahr) <= self.pd_todesjahr:
-                            re_article["TODESJAHR"].value = ""
-                            re_article["KEINE_SCHÖPFUNGSHÖHE"].value = False
-                    if geburtsjahr := re_article["GEBURTSJAHR"].value:
-                        if int(geburtsjahr) <= self.pd_geburtsjahr:
-                            re_article["GEBURTSJAHR"].value = ""
-                            re_article["KEINE_SCHÖPFUNGSHÖHE"].value = False
+        article = self.re_page.first_article
+        if article["KURZTEXT"].value:
+            return
+        try:
+            article["KURZTEXT"].value = self.short_description_lookup[self.re_page.lemma_without_prefix]
+            self.re_page.first_article.text += f"\n{self.MAINTENANCE_CAT}"
+        except KeyError:
+            pass
--- service/ws_re/scanner/tasks/test_add_short_description.py
+++ service/ws_re/scanner/tasks/test_add_short_description.py
@@ -1,12 +1,14 @@
+# pylint: disable=protected-access,no-self-use
+from unittest import mock, skip
+
+import pywikibot
 from testfixtures import compare
 
 from service.ws_re.scanner.tasks.add_short_description import KURZTask
 from service.ws_re.scanner.tasks.test_base_task import TaskTestCase
 from service.ws_re.template.re_page import RePage
 
-class TestKURZTaskProcessSource(TaskTestCase):
-    def test_load_short_descriptions_from_text(self):
-        source_short_text = """{|class="wikitable"
+TEXT_A = """{|class="wikitable"
 |-
 !Artikel!!Kurzbeschreibung
 |-
@@ -16,5 +18,67 @@ class TestKURZTaskProcessSource(TaskTestCase):
 |-
 |[[RE:No real description]]||(-)
 |}"""
-        short_text_lookup = KURZTask._load_source_short_description(source_short_text)
+
+TEXT_Z = """
+{|class="wikitable"
+|-
+!Artikel!!Kurzbeschreibung
+|-
+|[[RE:Zaa]]||Volk Aithiopiens
+|-
+|[[RE:Zaaram]]||Hauptstadt der Kinaidokolpitai an der W-Küste Arabiens
+|}"""
+
+class TestKURZTaskProcessSource(TaskTestCase):
+    # pylint: disable=arguments-differ
+    def setUp(self):
+        super().setUp()  # pylint: disable=no-value-for-parameter
+        self.page_fetcher_mock = mock.patch("service.ws_re.scanner.tasks.add_short_description."
+                                            "KURZTask._get_short_description_text_from_source").start()
+        self.alphabet_mock = mock.patch("service.ws_re.scanner.tasks.add_short_description.RE_ALPHABET",
+                                        ["a", "z"]).start()
+        self.page_fetcher_mock.return_value = TEXT_A
+        self.addCleanup(mock.patch.stopall)
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_load_from_source_pages(self):
+        task = KURZTask(None, self.logger)
+        self.page_fetcher_mock.side_effect = [TEXT_A, TEXT_Z]
+        union_dict = task._load_short_descriptions()
+        compare(union_dict["Aachen"], "deutsche Stadt = Aquae")
+        compare(union_dict["Zaa"], "Volk Aithiopiens")
+        compare(4, len(union_dict))
+
+    def test_load_short_descriptions_from_text(self):
+        short_text_lookup = KURZTask._parse_short_description(TEXT_A)
         compare(short_text_lookup, {"Aachen": "deutsche Stadt = Aquae", "Aal": "Zoologisch"})
+
+    def test_add_short_description_to_lemma(self):
+        self.text_mock.return_value = """{{REDaten}}
+text
+{{REAutor|Autor.}}"""
+        self.title_mock.return_value = "Re:Aachen"
+        re_page = RePage(self.page_mock)
+        task = KURZTask(None, self.logger)
+        compare({"success": True, "changed": True}, task.run(re_page))
+        compare("deutsche Stadt = Aquae", re_page.first_article["KURZTEXT"].value)
+        compare("text\n[[Kategorie:RE:Kurztext überprüfen]]", re_page.first_article.text)
+
+    def test_existing_short_description_to_lemma(self):
+        self.text_mock.return_value = """{{REDaten
+|KURZTEXT=Test}}
+{{REAutor|Autor.}}"""
+        self.title_mock.return_value = "Re:Aachen"
+        re_page = RePage(self.page_mock)
+        task = KURZTask(None, self.logger)
+        compare({"success": True, "changed": False}, task.run(re_page))
+        compare("Test", re_page.first_article["KURZTEXT"].value)
+
+
+@skip("only for analysis")
+class TestKURZTaskProcessSourceLoadReality(TaskTestCase):
+    def test_load_real_sources(self):
+        task = KURZTask(pywikibot.Site(code='de', fam='wikisource', user='THEbotIT'), self.logger)
+        self.assertGreater(len(task.short_description_lookup), 14000)
