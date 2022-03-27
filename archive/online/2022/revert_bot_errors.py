b673129f3990fcc3e4a4354c11157f39eaa98d69
Erik Sommer <ersotech@posteo.de>
2022-03-27 19:06:14 +0200

    full blown roll back algorithm

--- archive/online/2022/revert_bot_errors.py
+++ archive/online/2022/revert_bot_errors.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 
-from pywikibot import Site, Page, User
+from pywikibot import Site, Page, User, Timestamp
 from pywikibot.page import Revision
 
 from tools.bots.pi import OneTimeBot
@@ -10,18 +10,23 @@ class RevertBotDeletions(OneTimeBot):
     def task(self):
         the_bot_it_user = User(self.wiki, "Benutzer:THEbotIT")
         page: Optional[Page] = None
-        for page, revision_id, ts, _ in the_bot_it_user.contributions(total=1000):
+        for idx, (page, revision_id, ts, _) in enumerate(the_bot_it_user.contributions(total=10000, start=Timestamp(year=2022, month=3, day=26), end=Timestamp(year=2022, month=3, day=22))):
+            if idx % 100 == 0:
+                self.logger.info(f"index is {idx}")
+            if idx < 700:
+                continue
             last_two_reivisions: List[Revision] = list(page.revisions())[:2]
             # make sure the last edit is the potential dangerous one
             if last_two_reivisions[0]["revid"] == revision_id:
                 if last_two_reivisions[0]["size"] - last_two_reivisions[1]["size"] < 0:
                     if "RE:" in page.title():
-                        print(page)
-                        print("bad edit")
-        print("end")
+                        self.logger.info(f"bad edit on {page.title()}")
+                        page.text = page.getOldVersion(oldid=last_two_reivisions[1]["revid"])
+                        page.save(f"Änderung {last_two_reivisions[0]['revid']} von [[Benutzer:THEbotIT|THEbotIT]] ([[Benutzer Diskussion:THEbotIT|Diskussion]]) rückgängig gemacht.")
+        self.logger.info("THE END")
 
 
 if __name__ == "__main__":
-    WS_WIKI = Site(code="de", fam="wikisource", user="THE IT")
+    WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     with RevertBotDeletions(wiki=WS_WIKI, debug=True, log_to_screen=True, log_to_wiki=False) as bot:
         bot.run()
