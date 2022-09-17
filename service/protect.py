35856685d2dc8cb266a354e04ceaab68b6391c06
Erik Sommer <ersotech@posteo.de>
2022-09-17 20:54:28 +0200

    updated and scheduling

--- service/protect.py
+++ service/protect.py
@@ -11,7 +11,7 @@ class Protect(CanonicalBot):
     def __init__(self, wiki: Site = None, debug: bool = True,
                  log_to_screen: bool = True, log_to_wiki: bool = True):
         CanonicalBot.__init__(self, wiki, debug, log_to_screen, log_to_wiki)
-        self.timeout: timedelta = timedelta(hours=12)
+        self.timeout: timedelta = timedelta(minutes=30)
 
     def __enter__(self):
         super().__enter__()
@@ -54,7 +54,7 @@ class Protect(CanonicalBot):
                 self.logger.info(f"checked {idx + 1} lemmas")
                 self.logger.info(f"{protected_lemmas} lemmas protected")
                 self.logger.info(f"oldest_timestamp: {datetime.strptime(min(self.data.values()), '%Y%m%d%H%M%S')}")
-                self.logger.info(f"lemmas in storgae: {len(self.data)}")
+                self.logger.info(f"lemmas in storage: {len(self.data)}")
                 break
         return True
 
