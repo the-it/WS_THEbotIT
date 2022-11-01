2a967ae221d9c0748b029356cab3c6d16375129e
Erik Sommer <ersotech@posteo.de>
2022-11-01 23:51:05 +0100

    repair redirects (#856)

--- service/ws_re/scanner/tasks/check_redirect_links.py
+++ service/ws_re/scanner/tasks/check_redirect_links.py
@@ -3,6 +3,7 @@ import re
 import pywikibot
 
 from service.ws_re.scanner.tasks.base_task import ReScannerTask
+from tools import save_if_changed
 from tools.bots.pi import WikiLogger
 
 
@@ -12,20 +13,33 @@ class CHRETask(ReScannerTask):
     def __init__(self, wiki: pywikibot.Site, logger: WikiLogger, debug: bool = True):
         ReScannerTask.__init__(self, wiki, logger, debug)
         self.filter_regex = re.compile(
-            r"(Benutzer:S8w4|"
+            r"(Benutzer:|"
             r"Paulys Realencyclopädie der classischen Altertumswissenschaft/Register/|"
             r"Wikisource:RE-Werkstatt/"
             r").*"
         )
 
     def task(self) -> bool:
+        self.iterate_throw_redirects(repair=True)
+        self.iterate_throw_redirects(report=True)
+        return True
+
+    def iterate_throw_redirects(self, repair: bool = False, report: bool = False):
         for redirect in self.re_page.get_redirects():
             filtered_list = self.filter_link_list(self.get_backlinks(redirect))
             if filtered_list:
-                self.re_page.add_error_category(self.error_category)
-                return True
+                if repair:
+                    self.rename_redirects_to_target(filtered_list, redirect)
+                if report:
+                    self.re_page.add_error_category(self.error_category)
+                    return
         self.re_page.remove_error_category(self.error_category)
-        return True
+
+    def rename_redirects_to_target(self, filtered_list: list[str], redirect: pywikibot.Page):
+        for entry in filtered_list:
+            page = pywikibot.Page(self.wiki, entry)
+            new_text = self.replace_redirect_links(page.text, redirect.title()[3:], self.re_page.lemma_without_prefix)
+            save_if_changed(page, new_text, f"Link korrigiert von {redirect} zu {self.re_page.page}")
 
     @staticmethod
     def get_backlinks(redirect_page: pywikibot.Page) -> list[str]:
@@ -33,3 +47,13 @@ class CHRETask(ReScannerTask):
 
     def filter_link_list(self, link_list: list[str]) -> list[str]:
         return [link for link in link_list if self.filter_regex.search(link) is None]
+
+    @staticmethod
+    def replace_redirect_links(text: str, redirect: str, target: str):
+        # [[RE:Redirect]] -> [[RE:Target]], [[RE:Redirect|Something]] -> [[RE:Target|Something]]
+        temp_text = re.sub(rf"\[\[RE:{redirect}(\||\]\])", rf"[[RE:{target}\g<1>", text)
+        # {{RE siehe|Redirect}} -> {{RE siehe|Target|Redirect}}
+        temp_text = re.sub(rf"{{RE siehe\|{redirect}}}", f"{{RE siehe|{target}|{redirect}}}", temp_text)
+        # {{RE siehe|Redirect|Something}} -> {{RE siehe|Target|Something}}
+        temp_text = re.sub(rf"{{RE siehe\|{redirect}\|(.+?)}}", rf"{{RE siehe|{target}|\g<1>}}", temp_text)
+        return temp_text
--- service/ws_re/scanner/tasks/test_check_redirect_links.py
+++ service/ws_re/scanner/tasks/test_check_redirect_links.py
@@ -1,5 +1,6 @@
 # pylint: disable=protected-access
 import pywikibot
+from ddt import ddt, file_data
 from pywikibot import Site
 from testfixtures import compare
 
@@ -9,6 +10,7 @@ from service.ws_re.template.re_page import RePage
 from tools.test import real_wiki_test
 
 
+@ddt
 class TestCHRETask(TaskTestCase):
     @real_wiki_test
     def test_get_backlinks(self):
@@ -24,14 +26,21 @@ class TestCHRETask(TaskTestCase):
             "RE:Querverweis",
             "Wikisource:RE-Werkstatt/Zeug in der Werkstatt",
             "Benutzer:S8w4/Spielwiese/Lemmata06kurz",
+            "Benutzer:THEbotIT/some_logging_page"
             "Paulys Realencyclopädie der classischen Altertumswissenschaft/Register/PD 2013",
         ]
         task = CHRETask(None, self.logger)
-        compare(link_list[:2], task.filter_link_list(link_list))
+        compare(["Literatur", "RE:Querverweis"], task.filter_link_list(link_list))
 
     @real_wiki_test
     def test_integration(self):
         WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
         task = CHRETask(WS_WIKI, self.logger)
-        task.re_page = RePage(pywikibot.Page(WS_WIKI, "RE:ἐπὶ βωμῷ ἱερεύς, ὁ"))
+        task.re_page = RePage(pywikibot.Page(WS_WIKI, "RE:Abgar, Abgaros 9"))
         task.task()
+
+    @file_data("test_data/test_check_redirect_links.yml")
+    def test_replace_redirect_links(self, text, redirect, target, expect):
+        task = CHRETask(None, self.logger)
+        replaced_text = task.replace_redirect_links(text, redirect, target)
+        compare(expect, replaced_text)
