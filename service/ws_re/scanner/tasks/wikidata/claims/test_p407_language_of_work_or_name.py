efd3419a720a2ce9476ad5d530842b8cac07fc6e
Erik Sommer <ersotech@posteo.de>
2022-11-27 18:54:33 +0100

    add tests for p407

---
+++ service/ws_re/scanner/tasks/wikidata/claims/test_p407_language_of_work_or_name.py
@@ -0,0 +1,15 @@
+# pylint: disable=protected-access
+
+from testfixtures import compare
+
+from service.ws_re.scanner.tasks.wikidata.claims.p407_language_of_work_or_name import P407LanguageOfWorkOrName
+from service.ws_re.scanner.tasks.wikidata.claims.test_claim_factory import \
+    BaseTestClaimFactory
+
+
+class TestP31InstanceOf(BaseTestClaimFactory):
+    def test__get_claim_json(self):
+        re_page = self._create_mock_page(text="{{REDaten}}\ntext\n{{REAutor|Some Author.}}", title="RE:Bla")
+        factory = P407LanguageOfWorkOrName(re_page, None)
+        claim_json = factory._get_claim_json()
+        compare(188, claim_json[0]["mainsnak"]["datavalue"]["value"]["numeric-id"])
