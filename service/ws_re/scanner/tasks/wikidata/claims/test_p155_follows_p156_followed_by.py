6c3374259f159538a7a5747ef49eda6220aebd1b
Erik Sommer <ersotech@posteo.de>
2022-11-27 19:35:22 +0100

    quality

--- service/ws_re/scanner/tasks/wikidata/claims/test_p155_follows_p156_followed_by.py
+++ service/ws_re/scanner/tasks/wikidata/claims/test_p155_follows_p156_followed_by.py
@@ -15,13 +15,15 @@ class TestP155Follows(BaseTestClaimFactory):
         compare(0, len(claim_json))
 
     def test__get_claim_json_no_follows_lemma_exists(self):
-        re_page = self._create_mock_page(text="{{REDaten\n|VORGÄNGER=Vorwort (Band I)}}\ntext\n{{REAutor|Blub}}", title="RE:Bla")
+        re_page = self._create_mock_page(text="{{REDaten\n|VORGÄNGER=Vorwort (Band I)}}\ntext\n{{REAutor|Blub}}",
+                                         title="RE:Bla")
         factory = P155Follows(re_page, self.logger)
         claim_json = factory._get_claim_json()
         compare(33127714, claim_json[0]["mainsnak"]["datavalue"]["value"]["numeric-id"])
 
     def test__get_claim_json_no_followed_by_lemma_exists(self):
-        re_page = self._create_mock_page(text="{{REDaten\n|NACHFOLGER=Abkürzungen}}\ntext\n{{REAutor|Blub}}", title="RE:Bla")
+        re_page = self._create_mock_page(text="{{REDaten\n|NACHFOLGER=Abkürzungen}}\ntext\n{{REAutor|Blub}}",
+                                         title="RE:Bla")
         factory = P156FollowedBy(re_page, self.logger)
         claim_json = factory._get_claim_json()
         compare(33127715, claim_json[0]["mainsnak"]["datavalue"]["value"]["numeric-id"])
