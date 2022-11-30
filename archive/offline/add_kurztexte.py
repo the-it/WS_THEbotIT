2098ce8cbf5d6d7136a42dc9affba12a09b8f0ff
Erik Sommer <ersotech@posteo.de>
2022-11-30 16:40:55 +0100

    add kurztexte

---
+++ archive/offline/add_kurztexte.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from pywikibot import Site
+
+from service.ws_re.register.registers import Registers
+from service.ws_re.scanner.tasks.add_short_description import KURZTask
+from tools.bots.pi import WikiLogger
+
+if __name__ == "__main__":  # pragma: no cover
+    wiki = Site(code="de", fam="wikisource", user="THEbotIT")
+    logger = WikiLogger("", datetime.now())
+    task = KURZTask(wiki, logger)
+    registers = Registers()
+    for volume in registers.volumes.values():
+        for lemma in volume:
+            if lemma.exists or lemma.short_description:
+                continue
+            if lemma.sort_key in task.short_description_lookup:
+                lemma.update_lemma_dict(update_dict={"short_description": task.short_description_lookup[lemma.sort_key]})
+    registers.persist()
