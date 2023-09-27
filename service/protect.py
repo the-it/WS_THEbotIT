3834addd3949c490cbda8997473e5b3cf82c8073
Erik Sommer <ersotech@posteo.de>
2023-09-28 00:07:46 +0200

    handle lemmas which changed back from Fertig

--- service/protect.py
+++ service/protect.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from datetime import datetime, timedelta
 
 from pywikibot import Site, Page, exceptions
@@ -42,14 +43,24 @@ class Protect(CanonicalBot):
         lemma_list = searcher.get_combined_lemma_list(self.data)
         protected_lemmas = 0
         for idx, lemma_str in enumerate(lemma_list):
-            self.data[lemma_str] = datetime.now().strftime("%Y%m%d%H%M%S")
             lemma = Page(self.wiki, lemma_str)
+            categories: list[str] = list(lemma.categories())
+            fertig_cat = False
+            for category in categories:
+                if category.find("Fertig"):
+                    fertig_cat = True
+                    break
+            if not fertig_cat:
+                with suppress(KeyError):
+                    del self.data[lemma_str]
+                continue
+            self.data[lemma_str] = datetime.now().strftime("%Y%m%d%H%M%S")
             self.logger.debug(f"check lemma {lemma.title()} for protection")
             if not lemma.protection():
                 self.logger.debug(f"protect lemma {lemma.title()}")
                 try:
-                    lemma.protect(reason="Schutz fertiger Seiten",
-                                  protections={'move': 'autoconfirmed', 'edit': 'autoconfirmed'})
+                    # lemma.protect(reason="Schutz fertiger Seiten",
+                    #               protections={'move': 'autoconfirmed', 'edit': 'autoconfirmed'})
                     protected_lemmas += 1
                 except exceptions.APIError as error:
                     self.logger.error(f"Wasn't able to protect {lemma.title()}, error was {error}")
@@ -65,6 +76,8 @@ class Protect(CanonicalBot):
 # PYWIKIBOT_DIR=/home/esommer/.pywikibot_protect
 
 if __name__ == "__main__":
-    WS_WIKI = Site(code="de", fam="wikisource", user="THEprotectIT")
+    import os
+    print(os.environ["PYWIKIBOT_DIR"])
+    WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     with Protect(wiki=WS_WIKI, debug=False) as bot:
         bot.run()
