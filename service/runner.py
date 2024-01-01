b75f0102e70284064838b64d604a8458bbc524a0
Erik Sommer <ersotech@posteo.de>
2024-01-01 20:48:50 +0100

    enable for prd env

--- service/runner.py
+++ service/runner.py
@@ -10,12 +10,13 @@ from service.ws_re.register.printer import ReRegisterPrinter
 from service.ws_re.scanner.base import ReScanner
 from service.ws_re.status import ReStatus
 from tools.bot_scheduler import BotScheduler
+from tools.bots.cloud.poc_bot import Poc
 
 if __name__ == "__main__":
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())  # type: ignore
     WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     SCHEDULER = BotScheduler(wiki=WS_WIKI, debug=False)
-    SCHEDULER.daily_bots = [AuthorList, ReScanner, ReRegisterPrinter]
+    SCHEDULER.daily_bots = [AuthorList, ReScanner, ReRegisterPrinter, Poc]
     SCHEDULER.weekly_bots = {
         0: [],  # monday
         1: [],
--- tools/bots/cloud/base.py
+++ tools/bots/cloud/base.py
@@ -9,7 +9,7 @@ def is_aws_test_env() -> bool:
 def get_aws_credentials() -> Tuple[str, str]:
     if "WS_AWS_TST_ENV" in os.environ:
         return os.environ["WS_AWS_TST_KEY"], os.environ["WS_AWS_TST_SECRET"]
-    if "WS_AWS_PRD_ENV" in os.environ:
+    if "WS_AWS_PRD_KEY" in os.environ:
         return os.environ["WS_AWS_PRD_KEY"], os.environ["WS_AWS_PRD_SECRET"]
     # if no valid env is present, just go with bogo keys
     return "", ""
