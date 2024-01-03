f045f0aa0c639637b7e303cffd88e1a31d2a92b7
Erik Sommer <ersotech@posteo.de>
2024-01-04 00:11:06 +0100

    change start script

--- service/starter.sh
+++ service/starter.sh
@@ -10,7 +10,7 @@ git pull
 git reset --hard main
 sudo /usr/local/bin/python3 -m pip install -r requirements.txt
 export PYTHONPATH=${PYTHONPATH}:${BASE_DIR}
-source /etc/environment
+source /etc/profile
 /usr/local/bin/python3 service/runner.py
 export PYWIKIBOT_DIR=/home/pi/.pywikibot_protect/
 /usr/local/bin/python3 service/protect.py
