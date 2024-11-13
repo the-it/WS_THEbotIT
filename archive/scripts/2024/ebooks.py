9a334564378c93beb6ec2e56296fa10043407669
Erik Sommer <ersotech@posteo.de>
2024-11-14 00:23:46 +0100

    script to remove links to {{Vorlage:Ebooks}}

---
+++ archive/scripts/2024/ebooks.py
@@ -0,0 +1,32 @@
+import re
+
+from pywikibot import Site, Page
+
+from tools.bots.pi import OneTimeBot
+from tools.petscan import PetScan
+
+
+class Ebooks(OneTimeBot):
+    @staticmethod
+    def get_list():
+        searcher = PetScan()
+        searcher.add_yes_template("Ebooks")
+        searcher.add_namespace(0)
+        return searcher.run()
+
+    def task(self):
+        lemma_list = self.get_list()
+        for lemma in lemma_list:
+            page = Page(self.wiki, lemma['title'])
+            text = re.sub(r"( |<br ?\/>|\n|){{Ebooks\|[^}]+}}", "", page.text)
+            page.text = text
+            page.save("Vorlage Ebooks entfernt.")
+            print(page.title())
+        return True
+
+
+if __name__ == "__main__":
+    WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
+    WS_WIKI.login()
+    with Ebooks(wiki=WS_WIKI, debug=False, log_to_screen=True, log_to_wiki=False) as bot:
+        bot.run()
