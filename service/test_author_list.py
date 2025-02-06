ddfcb5e00be0716a709708c2fa46c2adf944760a
Erik Sommer <ersotech@posteo.de>
2025-02-06 16:45:52 +0100

    putting the new Authorlist bot live

--- service/test_author_list.py
+++ service/test_author_list.py
@@ -5,7 +5,7 @@ from ddt import file_data, ddt
 from pywikibot.scripts.generate_user_files import pywikibot
 from testfixtures import compare
 
-from service.author_list import AuthorListNew
+from service.author_list import AuthorList
 from tools.bots.cloud.test_base import TestCloudBase
 from tools.test import real_wiki_test
 
@@ -15,7 +15,7 @@ class TestAuthorList(TestCloudBase):
     wiki = pywikibot.Site(code="de", fam="wikisource", user="THEbotIT")
 
     def setUp(self):
-        self.author_list = AuthorListNew()
+        self.author_list = AuthorList()
 
     @file_data("test_data/test_get_page_infos.yml")
     def test_get_page_infos(self, given, expect):
@@ -52,7 +52,7 @@ class TestAuthorList(TestCloudBase):
         mock.patch("service.author_list.Page.save").start()
         lemma_mock.return_value = ([":Willy_Stöwer"], 1)
         self.addCleanup(mock.patch.stopall)
-        with AuthorListNew(self.wiki) as bot:
+        with AuthorList(self.wiki) as bot:
             bot.data.assign_dict({"checked": {}, "authors": {}})
             bot.run()
             compare(
