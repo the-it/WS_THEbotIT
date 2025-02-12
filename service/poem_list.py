0ccb9ee26e5db193d7e26ebb8a5a95ddcd1fa8cf
Erik Sommer <ersotech@posteo.de>
2025-02-12 22:40:40 +0100

    this will take some time

--- service/poem_list.py
+++ service/poem_list.py
@@ -5,6 +5,7 @@ from datetime import timedelta, datetime
 from math import ceil
 from typing import TypedDict, Literal, Tuple, Optional
 
+from authlib import author
 from pywikibot import ItemPage, Page, Site, Claim
 from pywikibot.exceptions import NoPageError
 
@@ -73,6 +74,8 @@ class PoemList(CloudBot):
             poem_dict["lemma"] = clean_lemma
             poem_dict["check"] = datetime.now().strftime("%Y%m%d%H%M%S")
             self.data[lemma] = poem_dict
+            if (idx + 50 > unprocessed_lemmas) and self._watchdog():
+                break
 
     def get_check_dict(self):
         return {poem: self.data[poem]["check"] for poem in self.data}
@@ -116,6 +119,52 @@ class PoemList(CloudBot):
         if template_value:
             poem_dict[info] = template_value
 
+    def sort_to_list(self) -> list[PoemDict]:
+        poem_list = [poem for poem in self.data.values()]
+        return sorted(poem_list, key=lambda poem_dict: (poem_dict["title"], poem_dict["lemma"]))
+
+    def print_poem_list(self, poem_list: list[PoemDict]) -> str:
+        start_of_run = self.status.current_run.start_time
+        string_list = []
+        string_list.append(f"Diese Liste der Gedichte enthält alle {len(self.data)}<ref>Stand: "
+                           f"{start_of_run.day}.{start_of_run.month}.{start_of_run.year}, "
+                           f"{start_of_run.strftime('%H:%M')} (UTC)</ref> Gedichte, "
+                           f"die in Wikisource digitalisiert wurden.")
+        string_list.append("Die Liste kann mit den Buttons neben den Spaltenüberschriften"
+                           " nach der jeweiligen Spalte sortiert werden.")
+        string_list.append("<!--")
+        string_list.append("Diese Liste wurde durch ein Computerprogramm erstellt, "
+                           "das die Daten verwendet, "
+                           "die aus den Infoboxen auf den Gedichtseiten stammen.")
+        string_list.append("Sollten daher Fehler vorhanden sein, "
+                           "sollten diese jeweils dort korrigiert werden.")
+        string_list.append("-->")
+        string_list.append("{|class=\"prettytable sortable tabelle-kopf-fixiert\"")
+        string_list.append("! Nr.")
+        string_list.append("! Autor")
+        string_list.append("! Titel")
+        string_list.append("! Gedichtanfang")
+        string_list.append("! Jahr")
+        string_list.append("! Stand")
+        for idx, poem_dict in enumerate(poem_list):
+            string_list.append("|-")
+            string_list.append(f"|{idx + 1}")
+            if "author" in poem_dict:
+                string_list.append(f"|{poem_dict[author]}")
+            else:
+                string_list.append(f"|")
+            title = poem_dict["lemma"]
+            if "title" in poem_dict:
+                if poem_dict["title"] != poem_dict["lemma"]:
+                    title = f"{poem_dict['title']}|"
+        string_list.append("|}")
+        string_list.append('')
+        string_list.append("== Fußnoten ==")
+        string_list.append("<references/>")
+        string_list.append('')
+        string_list.append("{{SORTIERUNG:Gedichte #Liste der}}")
+        string_list.append("[[Kategorie:Listen]]")
+        return "\n".join(string_list)
 
 if __name__ == "__main__":
     WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
