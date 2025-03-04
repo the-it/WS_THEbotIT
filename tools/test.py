b849eebe03f58bed2f7af4fe1a8020f4a301995e
Erik Sommer <ersotech@posteo.de>
2025-03-04 20:39:14 +0100

    split partitions

--- tools/test.py
+++ tools/test.py
@@ -3,10 +3,19 @@ from unittest import skipUnless, mock
 
 REAL_DATA_TEST = "WS_REAL_DATA" in os.environ
 REAL_WIKI_TEST = "WS_REAL_WIKI" in os.environ
+REAL_WIKI_PART = 0
+if "WS_REAL_WIKI_PART" in os.environ:
+    REAL_WIKI_PART = int(os.environ["WS_REAL_WIKI_PART"])
+REAL_WIKI_PARTITIONS = 1
+if "WS_REAL_WIKI_PARTITIONS" in os.environ:
+    REAL_WIKI_PARTITIONS = int(os.environ["WS_REAL_WIKI_PARTITIONS"])
 
 
 def real_wiki_test(func):
     wrapper = skipUnless(REAL_WIKI_TEST, "only execute in test against real wiki")(func)
+    if REAL_WIKI_PARTITIONS > 1:
+        wrapper = skipUnless(REAL_WIKI_TEST and abs(hash(func.__name__)) % REAL_WIKI_PARTITIONS == REAL_WIKI_PART,
+                             "test run in another partition")(func)
     return wrapper
 
 
