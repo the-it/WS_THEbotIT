c057c1bb61c8d3577b35e33dfee1eeedfacd3320
Erik Sommer <ersotech@posteo.de>
2025-03-29 19:07:41 +0100

    cleanup more stuff

--- legacy_tst_runner.py
+++
@@ -1,7 +0,0 @@
-import sys
-import unittest
-
-LOADER = unittest.TestLoader()
-SUITE = LOADER.discover(start_dir=".")
-RUNNER = unittest.TextTestRunner()
-sys.exit(not RUNNER.run(SUITE).wasSuccessful())
