352f60e392fa1dbfc41383e7d0086632bd7260af
Erik Sommer <ersotech@posteo.de>
2025-05-14 10:40:02 +0200

    pause new claim factory for the moment

--- service/ws_re/scanner/tasks/wikidata/claims/p13269_directs_readers_to.py
+++ service/ws_re/scanner/tasks/wikidata/claims/p13269_directs_readers_to.py
@@ -6,8 +6,6 @@ from service.ws_re.scanner.tasks.base import get_redirect
 from service.ws_re.scanner.tasks.wikidata.claims.claim_factory import ClaimFactory
 from service.ws_re.scanner.tasks.wikidata.claims._base import SnakParameter
 from service.ws_re.scanner.tasks.wikidata.claims._typing import JsonClaimDict
-from service.ws_re.template.re_page import RePage
-from tools.bots.pi import WikiLogger
 
 
 class P13269DirectsReadersTo(ClaimFactory):
@@ -15,21 +13,13 @@ class P13269DirectsReadersTo(ClaimFactory):
     Returns the Claim **directs readers to** -> **object of the RE lemma, that actually describes the topic**
     """
 
-    def __init__(self, re_page: RePage, logger: WikiLogger):
-        super().__init__(re_page, logger)
-        self.debug_counter = 0
-
     def _get_claim_json(self) -> List[JsonClaimDict]:
-        if self.debug_counter > 20:
-            # create only 20 items per night in the start phase
-            return []
         redirect = get_redirect(self.re_page.first_article)
         if redirect and isinstance(redirect, str):
             redirected_lemma = pywikibot.Page(self.re_page.page.site, f"RE:{redirect}")
             if redirected_lemma.exists():
                 data_item = redirected_lemma.data_item()
                 if data_item.exists():
-                    self.debug_counter += 1
                     self.logger.info(f"{self.re_page.lemma_as_link} -> {redirected_lemma.title(as_link=True)}")
                     snak_parameter = SnakParameter(property_str=self.get_property_string(),
                                                    target_type="wikibase-item",
