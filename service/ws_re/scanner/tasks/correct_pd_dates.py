b0707706c777eda721f8e725ddc892cc7a80a83c
Erik Sommer <ersotech@posteo.de>
2025-07-01 09:14:16 +0200

    chipping away

--- service/ws_re/scanner/tasks/correct_pd_dates.py
+++ service/ws_re/scanner/tasks/correct_pd_dates.py
@@ -19,21 +19,7 @@ class COPDTask(ReScannerTask):
     def task(self):
         for re_article_list in self.re_page.splitted_article_list:
             first_article = re_article_list[0]
-            years: dict[str, Optional[int]] = {"birth": None, "death": None, "pd":0}
-            for re_article in re_article_list:
-                if not isinstance(re_article, Article):
-                    continue
-                authors = self.authors.get_author_by_mapping(re_article.author.short_string, re_article["BAND"].value)
-                for author in authors:
-                    if author.year_public_domain > self.current_year and author.year_public_domain > years["pd"]:
-                        if author.death:
-                            years["birth"] = None
-                            years["death"] = author.death
-                            years["pd"] = author.year_public_domain
-                        else:
-                            years["birth"] = author.birth
-                            years["death"] = None
-                            years["pd"] = author.year_public_domain
+            years = self.get_max_pd_year(re_article_list)
             if years["death"]:
                 first_article["TODESJAHR"].value = str(years["death"])
                 first_article["GEBURTSJAHR"].value = ""
@@ -45,3 +31,21 @@ class COPDTask(ReScannerTask):
                 first_article["GEBURTSJAHR"].value = ""
                 first_article["KEINE_SCHÖPFUNGSHÖHE"].value = False
         return True
+
+    def get_max_pd_year(self, re_article_list) -> dict[str, Optional[int]]:
+        years: dict[str, Optional[int]] = {"birth": None, "death": None, "pd": 0}
+        for re_article in re_article_list:
+            if not isinstance(re_article, Article):
+                continue
+            authors = self.authors.get_author_by_mapping(re_article.author.short_string, re_article["BAND"].value)
+            for author in authors:
+                if author.year_public_domain > years["pd"]:
+                    if author.death:
+                        years["birth"] = None
+                        years["death"] = author.death
+                        years["pd"] = author.year_public_domain
+                    else:
+                        years["birth"] = author.birth
+                        years["death"] = None
+                        years["pd"] = author.year_public_domain
+        return years
--- service/ws_re/scanner/tasks/test_correct_pd_dates.py
+++ service/ws_re/scanner/tasks/test_correct_pd_dates.py
@@ -1,5 +1,4 @@
-from unittest import TestCase
-
+from freezegun import freeze_time
 from testfixtures import compare
 
 from service.ws_re.scanner.tasks.correct_pd_dates import COPDTask
@@ -7,8 +6,14 @@ from service.ws_re.scanner.tasks.test_base_task import TaskTestCase
 from service.ws_re.template.re_page import RePage
 
 
+@freeze_time("2025-01-01")
 class TestCOPDTask(TaskTestCase):
-    def test_remove_date(self):
+    def setUp(self):
+        super().setUp()
+        self.task = COPDTask(None, self.logger)
+        pass
+
+    def test_get_pd_death(self):
         self.page_mock.text = """{{REDaten
 |BAND=XII,1
 |KEINE_SCHÖPFUNGSHÖHE=ON
@@ -17,10 +22,19 @@ class TestCOPDTask(TaskTestCase):
 }}
 something
 {{REAutor|Obst.}}"""
-        self.page_mock.title_str = "Re:Labax"
         re_page = RePage(self.page_mock)
-        task = COPDTask(None, self.logger)
-        compare({"success": True, "changed": True}, task.run(re_page))
-        compare("", re_page.first_article["GEBURTSJAHR"].value)
-        compare("", re_page.first_article["TODESJAHR"].value)
-        compare(False, re_page.first_article["KEINE_SCHÖPFUNGSHÖHE"].value)
+        article_list = re_page.splitted_article_list[0]
+        compare({"birth": None, "death": 1944, "pd": 2015}, self.task.get_max_pd_year(article_list))
+
+    def test_get_pd_birth(self):
+        self.page_mock.text = """{{REDaten
+|BAND=XII,1
+|KEINE_SCHÖPFUNGSHÖHE=ON
+|TODESJAHR=
+|GEBURTSJAHR=
+}}
+something
+{{REAutor|Werner Eck.}}"""
+        re_page = RePage(self.page_mock)
+        article_list = re_page.splitted_article_list[0]
+        compare({"birth": 1939, "death": None, "pd": 2090}, self.task.get_max_pd_year(article_list))
