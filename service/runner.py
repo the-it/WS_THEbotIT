daa2570f4c1c5634cdf4ccf8490700a961ea094f
Erik Sommer <ersotech@posteo.de>
2025-08-11 15:12:30 +0200

    testing less privileges real quick

--- service/runner.py
+++ service/runner.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())  # type: ignore
     WS_WIKI = Site(code="de", fam="wikisource", user="THEbotIT")
     SCHEDULER = BotScheduler(wiki=WS_WIKI, debug=False)
-    SCHEDULER.daily_bots = [AuthorList, PoemList, ReScanner, ReRegisterPrinter, Finisher]
+    #SCHEDULER.daily_bots = [AuthorList, PoemList, ReScanner, ReRegisterPrinter, Finisher]
+    SCHEDULER.daily_bots = [ReScanner]
     SCHEDULER.weekly_bots = {
         0: [],  # monday
         1: [],
