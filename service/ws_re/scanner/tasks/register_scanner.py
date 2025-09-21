8af71cf6cb496e7a881893e195d7bb60caa4dec4
Erik Sommer <ersotech@posteo.de>
2025-09-21 16:10:24 +0200

    Update register_scanner.py

--- service/ws_re/scanner/tasks/register_scanner.py
+++ service/ws_re/scanner/tasks/register_scanner.py
@@ -48,7 +48,7 @@ class SCANTask(ReScannerTask):
     def _fetch_wp_link(self, article_list: List[Article]) -> Tuple[LemmaDict, UpdaterRemoveList]:
         article = article_list[0]
         if wp_value := article['WIKIPEDIA'].value:
-            wp_link: Optional[str] = f"w:de:{wp_value}" if not wp_value.contains(":") else "w:{wp_value}"
+            wp_link: Optional[str] = f"w:de:{wp_value}" if ":" not in wp_value else "w:{wp_value}"
         else:
             wp_link = self._get_link_from_wd(("dewikipedia", "enwikipedia", "frwikipedia", "itwikipedia",
                                               "eswikipedia", "ptwikipedia", "sewikipedia", "cawikipedia",
