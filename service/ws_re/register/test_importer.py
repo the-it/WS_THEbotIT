73ba42e566cc0dda5335c19e500909f1dfb6c7e6
Erik Sommer <ersotech@posteo.de>
2025-10-21 09:39:27 +0200

    adjust author string to reduce manual work later

---
+++ service/ws_re/register/test_importer.py
@@ -0,0 +1,21 @@
+# pylint: disable=protected-access,no-self-use
+from unittest import TestCase
+
+from ddt import ddt, file_data
+from testfixtures import compare
+
+from service.ws_re.register.importer import ReImporter
+
+
+@ddt
+class TestReImporter(TestCase):
+    @staticmethod
+    def test_get_author_mapping():
+        mapping = ReImporter.get_author_mapping()
+        compare("Stein.", mapping["Arthur Stein"])
+        compare("A.W.", mapping["Albert Wünsch"])
+
+    @file_data("test_data/test_importer.yml")
+    def test_adjust_author(self, given, expect):
+        mapping = ReImporter.get_author_mapping()
+        compare(expect, ReImporter.adjust_author(given, mapping))
