b9623a61dd8916c85a92df136b1f499853f9aae2
Erik Sommer <ersotech@posteo.de>
2026-07-08 20:31:20 +0200

    tweak start script to include uv's install dir

--- service/starter.sh
+++ service/starter.sh
@@ -9,10 +9,11 @@ git checkout main
 git pull
 git reset --hard main
 rm -rf service/ws_re/register/data
-sudo /usr/local/bin/python3 -m pip install --user uv
 uv sync
 export PYTHONPATH=${PYTHONPATH}:${BASE_DIR}
 source /etc/profile
+# /etc/profile overwrites PATH, re-add uv's install dir
+export PATH="$HOME/.local/bin:$PATH"
 uv run service/runner.py
 export PYWIKIBOT_DIR=/home/pi/.pywikibot_protect/
 uv run service/protect.py
