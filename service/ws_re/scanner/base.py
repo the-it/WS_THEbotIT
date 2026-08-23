7125415897bde30b825e0c6e3e7de734f4831932
Erik Sommer <ersotech@posteo.de>
2026-08-23 11:30:41 +0900

    More speed

--- service/ws_re/scanner/base.py
+++ service/ws_re/scanner/base.py
@@ -102,7 +102,7 @@ class ReScanner(CloudBot):
     @property
     def lemma_list(self) -> list[str]:
         searcher = self._prepare_searcher()
-        result, _ = searcher.get_combined_lemma_list(self.data, timeframe=168)
+        result, _ = searcher.get_combined_lemma_list(self.data, timeframe=25)
         return result
 
     def _activate_tasks(self) -> list[ReScannerTask]:
